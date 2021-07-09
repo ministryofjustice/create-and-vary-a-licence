2fe04f8c18cfc923c738f7fc0576eb0562331ccf
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2021-07-09 15:03:41 +0100

    Alter readme, docker-compose, landing page content and tests for it.

--- integration_tests/pages/index.js
+++ integration_tests/pages/index.js
@@ -1,7 +1,7 @@
 const page = require('./page')
 
 const indexPage = () =>
-  page('This site is under construction...', {
+  page('Welcome', {
     headerUserName: () => cy.get('[data-qa=header-user-name]'),
   })
 
