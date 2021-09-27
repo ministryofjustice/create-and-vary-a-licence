acd211525c6304d3979ea0be057524fe4923536e
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2021-09-27 17:51:51 +0100

    CVSL-102: Update the contact telephone number for the officer

--- server/routes/creatingLicences/handlers/initialMeetingContact.ts
+++ server/routes/creatingLicences/handlers/initialMeetingContact.ts
@@ -1,12 +1,17 @@
 import { Request, Response } from 'express'
+import LicenceService from '../../../services/licenceService'
 
 export default class InitialMeetingContactRoutes {
+  constructor(private readonly licenceService: LicenceService) {}
+
   GET = async (req: Request, res: Response): Promise<void> => {
-    res.render('pages/create/initialMeetingContact', {})
+    res.render('pages/create/initialMeetingContact')
   }
 
   POST = async (req: Request, res: Response): Promise<void> => {
     const { licenceId } = req.params
+    const { username } = res.locals.user
+    await this.licenceService.updateContactNumber(licenceId, req.body, username)
     res.redirect(`/licence/create/id/${licenceId}/initial-meeting-time`)
   }
 }
