7721c67e9cc4b06eea310283acdd33e5a16c3d38
Stephen McVeigh <smcveigh941@gmail.com>
2021-10-15 12:31:13 +0100

    CVSL-106: Add the rest of the additional conditions, giving them IDs. Remove conditional form builder logic for now

--- server/config/conditions.ts
+++ server/config/conditions.ts
@@ -41,15 +41,245 @@ export default {
   },
   additionalConditions: [
     {
-      id: 'placeOfResidence',
+      id: '48fb5aee-ed76-42ed-a99a-6195833fede4',
       groupName: 'Residence at a specific place',
       text: 'You must reside within [INSERT REGION] while of no fixed abode, unless otherwise approved by your supervising officer.',
-      inputTemplate: 'placeOfResidence',
     },
     {
-      id: 'attendAppointments',
+      id: '30b49bf8-4898-4378-9684-545b720dc909',
+      groupName: 'Restriction of residency',
+      text: 'Not to reside (not even to stay for one night) in the same household as [ANY / ANY FEMALE / ANY MALE] child under the age of [INSERT AGE] without the prior approval of your supervising officer.',
+    },
+    {
+      id: '840f097c-863b-45b6-9960-f235570cc491',
       groupName: 'Making or maintaining contact with a person',
       text: 'Attend all appointments arranged for you with a psychiatrist / psychologist / medical practitioner.',
+    },
+    {
+      id: '765bd487-2acf-4988-b83b-c5c49520dc45',
+      groupName: 'Making or maintaining contact with a person',
+      text: 'Receive home visits from a Mental Health Worker.',
+    },
+    {
+      id: '384de27d-f382-41eb-8b34-74551d3b16d1',
+      groupName: 'Making or maintaining contact with a person',
+      text: 'Attend [INSERT APPOINTMENT TIME DATE AND ADDRESS], as directed, to address your dependency on, or propensity to misuse, a controlled drug.',
+    },
+    {
+      id: '45bed19e-7b1b-4166-bebc-6ab4e5283ceb',
+      groupName: 'Making or maintaining contact with a person',
+      text: 'Should you return to the UK and Islands before the expiry date of your licence then your licence conditions will be in force and you must report within two working days to your supervising officer.',
+    },
+    {
+      id: '766971f3-5212-4426-81de-3e3e8b300a15',
+      groupName: 'Making or maintaining contact with a person',
+      text: 'Not to seek to approach or communicate with [INSERT NAME OF VICTIM AND / OR FAMILY MEMBERS] without the prior approval of your supervising officer and / or [INSERT NAME OF APPROPRIATE SOCIAL SERVICES DEPARTMENT].',
+    },
+    {
+      id: '5b116ba6-fe89-41a2-9333-2f98881d3c69',
+      groupName: 'Making or maintaining contact with a person',
+      text: 'Not to have unsupervised contact with [ANY / ANY FEMALE / ANY MALE] children under the age of [INSERT AGE] without the prior approval of your supervising officer and / or [INSERT NAME OF APPROPRIATE SOCIAL SERVICES DEPARTMENT] except where that contact is inadvertent and not reasonably avoidable in the course of lawful daily life.',
+    },
+    {
+      id: '4c6c7a93-9b79-48ad-9126-40fe2042af97',
+      groupName: 'Making or maintaining contact with a person',
+      text: 'Not to contact or associate with [NAMED OFFENDER(S) / NAMED INDIVIDUAL(S)] without the prior approval of your supervising officer.',
+    },
+    {
+      id: 'b191ec1f-b06c-4aa1-9f25-d0ac66d34a02',
+      groupName: 'Making or maintaining contact with a person',
+      text: 'Not to contact or associate with a known sex offender other than when compelled by attendance at a Treatment Programme or when residing at Approved Premises without the prior approval of your supervising officer.',
+    },
+    {
+      id: '9448a0a0-567f-4b85-8d9c-6efa65fea999',
+      groupName: 'Making or maintaining contact with a person',
+      text: 'Not to contact directly or indirectly any person who is a serving or remand prisoner or detained in State custody, without the prior approval of your supervising officer.',
+    },
+    {
+      id: '068f217d-17ed-4789-9422-692e2e34ee61',
+      groupName: 'Making or maintaining contact with a person',
+      text: 'Not to associate with any person currently or formerly associated with [NAME OF DESCRIBE SPECIFIC GROUPS OR ORGANISATIONS] without the prior approval of your supervising officer.',
+    },
+    {
+      id: '59d8b38f-ce07-471c-ad02-f1089b5fb24c',
+      groupName: 'Participation in, or co-operation with, a programme or set of activities',
+      text: 'To comply with any requirements specified by your supervising officer for the purpose of ensuring that you address your alcohol / drug / sexual / violent / gambling / solvent abuse / anger / debt / prolific / offending behaviour problems.',
+    },
+    {
+      id: 'ab72005d-d998-4990-a480-1eb6aa084c31',
+      groupName: 'Participation in, or co-operation with, a programme or set of activities',
+      text: 'Not to undertake work or other organised activity which will involve a person under the age of [INSERT AGE], either on a paid or unpaid basis without the prior approval of your supervising officer;.',
+    },
+    {
+      id: '0d0605cd-7679-4fdf-8fe8-0a5b90bafd46',
+      groupName: 'Possession, ownership, control or inspection of specified items or documents',
+      text: 'Not to own or possess more than one mobile phone or SIM card without the prior approval of your supervising officer and to provide your supervising officer with details of that mobile telephone or one you have regular use of, including the IMEI number and the SIM card that you possess.',
+    },
+    {
+      id: 'a5560b80-bfbd-47d4-aa1c-4f726cc0cddd',
+      groupName: 'Possession, ownership, control or inspection of specified items or documents',
+      text: 'Not to own or possess a mobile phone with a photographic function without the prior approval of your supervising officer.',
+    },
+    {
+      id: '53ca8c00-0656-4946-affe-a3ffdb15d07b',
+      groupName: 'Possession, ownership, control or inspection of specified items or documents',
+      text: 'Not to own or use a camera without the prior approval of your supervising officer.',
+    },
+    {
+      id: '95ec2e2c-33c5-47ca-8719-0826691fe50c',
+      groupName: 'Possession, ownership, control or inspection of specified items or documents',
+      text: 'To make any device capable of making or storing digital images (including a camera and a mobile phone with a camera function) available for inspection on request by your supervising officer and/or a police officer.',
+    },
+    {
+      id: 'da64d752-4843-4692-9cfb-4ca9c3efd88d',
+      groupName: 'Possession, ownership, control or inspection of specified items or documents',
+      text: 'To surrender your passport(s) to your supervising officer and to notify your supervising officer of any intention to apply for a new passport.',
+    },
+    {
+      id: 'c85bb6f2-4ece-4164-8646-dc6b34ef27fc',
+      groupName: 'Possession, ownership, control or inspection of specified items or documents',
+      text: 'Not to use or access any computer or device which is internet enabled without the prior approval of your supervising officer; and only for the purpose, and only at a specific location, as specified by that officer.',
+    },
+    {
+      id: '67895e46-6b9f-4b1d-9b21-b3b41478daf9',
+      groupName: 'Possession, ownership, control or inspection of specified items or documents',
+      text: 'Not to delete the usage history on any internet enabled device or computer used and to allow such items to be inspected as required by the police or your supervising officer. Such inspection may include removal of the device for inspection and the installation of monitoring software.',
+    },
+    {
+      id: '81d27331-7e5f-45bd-93f9-0e7f3c0c39a2',
+      groupName: 'Possession, ownership, control or inspection of specified items or documents',
+      text: 'Not to own or possess a [SPECIFIED ITEM) without the prior approval of your supervising officer.',
+    },
+    {
+      id: '2b68be7f-04a8-4fff-98bb-31ae5f2d0616',
+      groupName: 'Disclosure of information',
+      text: 'Provide your supervising officer with details (such as make, model, colour, registration) of any vehicle you own, hire for more than a short journey or have regular use of, prior to any journey taking place.',
+    },
+    {
+      id: '9c05f100-1a0b-4bc7-9824-30182a29869d',
+      groupName: 'Disclosure of information',
+      text: 'Notify your supervising officer of any developing intimate relationships with [WOMEN / MEN / WOMEN OR MEN].',
+    },
+    {
+      id: '57855867-844b-4f36-b15d-34d1c65f6e85',
+      groupName: 'Disclosure of information',
+      text: 'Notify your supervising officer of any developing personal relationships, whether intimate or not, with any person you know or believe to be resident in a household containing children under the age of 18. This includes persons known to you prior to your time in custody with whom you are renewing or developing a personal relationship with.',
+    },
+    {
+      id: '1d34479c-f478-4038-b839-6c2fe19817fc',
+      groupName: 'Disclosure of information',
+      text: 'To notify your supervising officer of the details of any passport that you possess (including passport number), and of any intention to apply for a new passport.',
+    },
+    {
+      id: '6cc810f9-4e56-4b96-95f8-0d31cf510f3b',
+      groupName: 'Disclosure of information',
+      text: 'Provide your supervising officer with the details of any bank accounts to which you are a signatory and of any credit cards you possess. You must also notify your supervising officer when becoming a signatory to any new bank account or credit card, and provide the account/card details. This condition will be reviewed on a monthly basis and may be amended or removed if it is felt that the level of risk that you present has reduced appropriately.',
+    },
+    {
+      id: '814b14af-d577-40c9-a161-c78c93a0faf5',
+      groupName: 'Curfew arrangement',
+      text: 'Confine yourself to an address approved by your supervising officer between the hours of [TIME] and [TIME] daily unless otherwise authorised by your supervising officer. This condition will be reviewed by your supervising officer on a [WEEKLY / MONTHLY / ETC] basis and may be amended or removed if it is felt that the level of risk that you present has reduced appropriately.',
+    },
+    {
+      id: 'eceba5b9-a938-46bf-828b-839d3da7b0ee',
+      groupName: 'Curfew arrangement',
+      text: 'Confine yourself to remain at [CURFEW ADDRESS] initially from [START OF CURFEW HOURS] until [END OF CURFEW HOURS] each day, and, thereafter, for such a period as may be reasonably notified to you by your supervising officer; and comply with such arrangements as may be reasonably put in place and notified to you by your supervising officer so as to allow for your whereabouts and your compliance with your curfew requirement be monitored (whether by electronic means involving your wearing an electronic tag or otherwise).',
+    },
+    {
+      id: 'b7fc574c-c735-49eb-bd63-d1d1cd9d370b',
+      groupName: 'Freedom of movement',
+      text: 'Not to enter the area of [CLEARLY SPECIFIED AREA], as defined by the attached map without the prior approval of your supervising officer.',
+    },
+    {
+      id: '87a5a387-87ec-4779-8bc4-1c684fddceed',
+      groupName: 'Freedom of movement',
+      text: 'Not to enter [NAME/TYPE OF PREMISES / ADDRESS / ROAD] without the prior approval of your supervising officer.',
+    },
+    {
+      id: '243b1ba5-1909-4382-a176-4272f934bc3a',
+      groupName: 'Freedom of movement',
+      text: "Not to enter or remain in sight of any [CHILDREN'S PLAY AREA, SWIMMING BATHS, SCHOOL ETC] without the prior approval of your supervising officer.",
+    },
+    {
+      id: '1f026506-5c5a-4e22-a969-35b96350e184',
+      groupName: 'Freedom of movement',
+      text: 'On release to be escorted by police to Approved Premises.',
+    },
+    {
+      id: '8ce68a6b-7ede-48d6-9f62-f28d7a7630ce',
+      groupName: 'Freedom of movement',
+      text: 'To only attend places of worship which have been previously agreed with your supervising officer.',
+    },
+    {
+      id: '209e73ad-7454-40db-907c-2833a62f503d',
+      groupName:
+        'Supervision in the community by the supervising officer, or other responsible officer, or organisation',
+      text: 'Report to staff at [NAME OF APPROVED PREMISES / POLICE STATION] at [TIME / DAILY], unless otherwise authorised by your supervising officer. This condition will be reviewed by your supervising officer on a [WEEKLY / MONTHLY / ETC] basis and may be amended or removed if it is felt that the level of risk you present has reduced appropriately.',
+    },
+    {
+      id: 'aefb33cf-9b5a-40f0-b51c-6029d1ebd1eb',
+      groupName: 'Restriction of specified conduct or specified acts',
+      text: 'Not to participate directly or indirectly in organising and/or contributing to any demonstration, meeting, gathering or website without the prior approval of your supervising officer. This condition will be reviewed on a monthly basis and may be amended or removed if your risk is assessed as having changed.',
+    },
+    {
+      id: 'b4678ee6-db5b-42b2-9fda-283bf5752878',
+      groupName: 'Extremism',
+      text: 'Not to contact directly or indirectly any person whom you know or believe to have been charged or convicted of any extremist related offence, without the prior approval of your supervising officer.',
+    },
+    {
+      id: '0ce8a553-6bf9-4849-b097-5692dff95908',
+      groupName: 'Extremism',
+      text: 'Not to attend or organise any meetings or gatherings other than those convened solely for the purposes of worship without the prior approval of your supervising officer.',
+    },
+    {
+      id: '048a9536-2a01-4272-b716-50bf98a3fe96',
+      groupName: 'Extremism',
+      text: 'Not to give or engage in the delivery of any lecture, talk, or sermon whether part of an act of worship or not, without the prior approval of your supervising officer.',
+    },
+    {
+      id: '153f752d-b9e6-49c1-ba26-24b09a714fd1',
+      groupName: 'Extremism',
+      text: 'Not to engage in any discussion or act to promote grooming or influencing of an individual or a group for the purpose of extremism or radicalisation.',
+    },
+    {
+      id: 'd2772c0d-ca65-41d3-8b9d-3c523ccb122f',
+      groupName: 'Extremism',
+      text: 'Not to have in your possession any printed or electronically recorded material or handwritten notes which contain encoded information or that promote the destruction of or hatred for any religious or ethnic group or that celebrates, justifies or promotes acts of violence, or that contain information about military or paramilitary technology, weapons, techniques or tactics without the prior approval of your supervising officer.',
+    },
+    {
+      id: 'baee07ba-6cbb-4ea5-9049-adf7cdd29eed',
+      groupName: 'Polygraph',
+      text: 'To comply with any instruction given by your supervising officer requiring you to attend polygraph testing. To participate in polygraph sessions and examinations as instructed by or under the authority of your supervising officer and to comply with any instruction given to you during a polygraph session by the person conducting the polygraph.',
+    },
+    {
+      id: '5b3047c3-ac1d-42db-93c5-581af4782a79',
+      groupName: 'Drug testing',
+      text: 'Attend [INSERT NAME AND ADDRESS], as reasonably required by your supervising officer, to give a sample of oral fluid / urine in order to test whether you have any specified Class A and specified Class B drugs in your body, for the purpose of ensuring that you are complying with the condition of your licence requiring you to be of good behaviour.',
+    },
+    {
+      id: 'ba7b5f64-860a-4ddc-ac35-b8daa1c76ddf',
+      groupName: 'Drug testing',
+      text: 'Not to take any action that could hamper or frustrate the drug testing process.',
+    },
+    {
+      id: '44150099-1e24-433e-a751-874f8865b945',
+      groupName: 'Electronic monitoring',
+      text: 'Allow person(s) as designated by your supervising officer to install an electronic monitoring tag on you and access to install any associated equipment in your property, and for the purpose of ensuring that equipment is functioning correctly. You must not damage or tamper with these devices and ensure that the tag is charged, and report to your supervising officer and the EM provider immediately if the tag or the associated equipment are not working correctly. This will be for the purpose of monitoring your [INSERT TYPES OF CONDITIONS TO BE ELECTRONICALLY MONITORED HERE] licence condition(s) unless otherwise authorised by your supervising officer.',
+    },
+    {
+      id: 'f99ff098-06eb-4ceb-a6fb-6d8bef4ce699',
+      groupName: 'Electronic monitoring',
+      text: 'You will be subject to trail monitoring. Your whereabouts will be electronically monitored by GPS Satellite Tagging, ending on [INSERT END DATE], and you must cooperate with the monitoring as directed by your supervising officer unless otherwise authorised by your supervising officer. .',
+    },
+    {
+      id: 'b90e3af2-b255-4b06-bf1e-d9b55dda40a4',
+      groupName: 'Electronic monitoring',
+      text: 'You must stay at [approved address] between 5pm and midnight every day until your electronic tag is installed unless otherwise authorised by your supervising officer.',
+    },
+    {
+      id: 'a6ec4e54-32f0-49a8-a0b0-29322d331a9c',
+      groupName: 'Electronic monitoring',
+      text: 'You are subject to alcohol monitoring. Your alcohol intake will be electronically monitoring for a period of [INSERT TIMEFRAME AND END DATE], and you may not consume units of alcohol, unless otherwise permitted by your supervising officer.',
     },
   ],
 }
