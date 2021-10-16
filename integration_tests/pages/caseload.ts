5841961927747d73f00cf5514b3c03bc26898466
timharrison-moj <47779388+timharrison-moj@users.noreply.github.com>
2021-10-16 17:06:16 +0100

    CVSL-166 Style tidy-up on caseload and approval journeys (#72)
    * CVS-166: Using links in place of buttons for create caseload. Adding back links to approval journey. Tidying styles for links.
    
    * CVSL-166: Fix for the disappearing columns SCSS applying outside of the spike caseload table.

--- integration_tests/pages/caseload.ts
+++ integration_tests/pages/caseload.ts
@@ -2,7 +2,7 @@ import Page from './page'
 import AppointmentPersonPage from './appointmentPerson'
 
 export default class CaseloadPage extends Page {
-  private createLicenceButtonId = '#create-licence-1'
+  private createLicenceButtonId = '#name-1'
 
   constructor() {
     super('caseload-page')
