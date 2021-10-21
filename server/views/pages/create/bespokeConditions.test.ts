8769cc41875b1b6ac3eb949434abc4bf7238b06d
Stephen McVeigh <smcveigh941@gmail.com>
2021-10-21 15:33:57 +0100

    CVSL-181: Fix bug where all bespoke condtions textboxes could be removed after arriving from check your answers, and submission caused internal server error (#76)

---
+++ server/views/pages/create/bespokeConditions.test.ts
@@ -0,0 +1,81 @@
+import fs from 'fs'
+import cheerio from 'cheerio'
+import nunjucks, { Template } from 'nunjucks'
+import { registerNunjucks } from '../../../utils/nunjucksSetup'
+
+const snippet = fs.readFileSync('server/views/pages/create/bespokeConditions.njk')
+
+describe('Create a Licence Views - Bespoke Conditions', () => {
+  let compiledTemplate: Template
+  let viewContext: Record<string, unknown>
+
+  const njkEnv = registerNunjucks()
+
+  beforeEach(() => {
+    compiledTemplate = nunjucks.compile(snippet.toString(), njkEnv)
+    viewContext = {}
+  })
+
+  it('should display just one textarea if no conditions exist on the licence already', () => {
+    viewContext = {
+      conditions: undefined,
+    }
+
+    const $ = cheerio.load(compiledTemplate.render(viewContext))
+
+    expect($('textarea').length).toBe(1)
+  })
+
+  it('should not display a remove button if no conditions exist on the licence already', () => {
+    viewContext = {
+      conditions: undefined,
+    }
+
+    const $ = cheerio.load(compiledTemplate.render(viewContext))
+
+    expect($('.moj-add-another__remove-button').length).toBe(0)
+  })
+
+  it('should display just one populated textarea if only 1 condition exists on the licence', () => {
+    viewContext = {
+      conditions: ['bespokeCondition1'],
+    }
+
+    const $ = cheerio.load(compiledTemplate.render(viewContext))
+
+    expect($('textarea').length).toBe(1)
+    expect($('#condition-0').text()).toBe('bespokeCondition1')
+  })
+
+  it('should not display a remove button if just one condition exists on the licence', () => {
+    viewContext = {
+      conditions: ['bespokeCondition1'],
+    }
+
+    const $ = cheerio.load(compiledTemplate.render(viewContext))
+
+    expect($('.moj-add-another__remove-button').length).toBe(0)
+  })
+
+  it('should display two populated textareas if 2 conditions exist on the licence', () => {
+    viewContext = {
+      conditions: ['bespokeCondition1', 'bespokeCondition2'],
+    }
+
+    const $ = cheerio.load(compiledTemplate.render(viewContext))
+
+    expect($('textarea').length).toBe(2)
+    expect($('#condition-0').text()).toBe('bespokeCondition1')
+    expect($('#condition-1').text()).toBe('bespokeCondition2')
+  })
+
+  it('should display two remove buttons if 2 conditions exist on the licence', () => {
+    viewContext = {
+      conditions: ['bespokeCondition1', 'bespokeCondition2'],
+    }
+
+    const $ = cheerio.load(compiledTemplate.render(viewContext))
+
+    expect($('.moj-add-another__remove-button').length).toBe(2)
+  })
+})
