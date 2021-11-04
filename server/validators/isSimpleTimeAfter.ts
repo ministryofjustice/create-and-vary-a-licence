d25efd99a520ee861177b15fe2c2c94984e98d92
Stephen McVeigh <smcveigh941@gmail.com>
2021-11-04 22:27:33 +0000

    CVSL-184: Unit test the validation decorator

--- server/validators/isSimpleTimeAfter.ts
+++ server/validators/isSimpleTimeAfter.ts
@@ -1,25 +1,13 @@
-import { registerDecorator, ValidationOptions, ValidationArguments } from 'class-validator'
+import { ValidationArguments } from 'class-validator'
 import SimpleTime from '../routes/creatingLicences/types/time'
 
-export default function IsSimpleTimeAfter(property: string, validationOptions?: ValidationOptions) {
-  return (object: unknown, propertyName: string) => {
-    registerDecorator({
-      name: 'isSimpleTimeAfter',
-      target: object.constructor,
-      propertyName,
-      constraints: [property],
-      options: validationOptions,
-      validator: {
-        validate(value: SimpleTime, args: ValidationArguments) {
-          const [relatedPropertyName] = args.constraints
-          const relatedValue = (args.object as SimpleTime)[relatedPropertyName]
-          return (
-            value instanceof SimpleTime &&
-            relatedValue instanceof SimpleTime &&
-            value.toMoment().isAfter(relatedValue.toMoment())
-          )
-        },
-      },
-    })
+export default function isSimpleTimeAfter(value: SimpleTime, args: ValidationArguments) {
+  const [relatedPropertyName] = args.constraints
+  const relatedValue = args.object[relatedPropertyName]
+
+  if (!(relatedValue instanceof SimpleTime)) {
+    throw new Error('The field supplied for comparison was not of the required type i.e. SimpleTime')
   }
+
+  return value.toMoment().isAfter(relatedValue.toMoment())
 }
