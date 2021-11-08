f867c46ec2b183c2642d70df2e79f33825e850d7
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2021-11-08 18:01:41 +0000

    CVSL-207: Removing unused PDF spike code. Now replaced with real documents.

--- server/routes/viewingLicences/index.ts
+++ server/routes/viewingLicences/index.ts
@@ -1,10 +1,9 @@
 import { RequestHandler, Router } from 'express'
 import asyncMiddleware from '../../middleware/asyncMiddleware'
-import PdfRoutes from './handlers/pdf'
 import { Services } from '../../services'
-import ViewAndPrintCaseRoutes from './handlers/viewCases'
 import fetchLicence from '../../middleware/fetchLicenceMiddleware'
 import validationMiddleware from '../../middleware/validationMiddleware'
+import ViewAndPrintCaseRoutes from './handlers/viewCases'
 import ViewAndPrintLicenceRoutes from './handlers/viewLicence'
 import PrintLicenceRoutes from './handlers/printLicence'
 
@@ -18,7 +17,6 @@ export default function Index({ licenceService, prisonerService }: Services): Ro
   const post = (path: string, handler: RequestHandler, type?: new () => unknown) =>
     router.post(routePrefix(path), validationMiddleware(type), asyncMiddleware(handler))
 
-  // These are REAL routes
   const viewCasesHandler = new ViewAndPrintCaseRoutes(licenceService)
   const viewLicenceHandler = new ViewAndPrintLicenceRoutes(licenceService)
   const printHandler = new PrintLicenceRoutes(prisonerService)
@@ -28,12 +26,6 @@ export default function Index({ licenceService, prisonerService }: Services): Ro
   get('/id/:licenceId/show', viewLicenceHandler.GET)
   get('/id/:licenceId/html-print', printHandler.preview)
   get('/id/:licenceId/pdf-print', printHandler.renderPdf)
-
-  // These are SPIKE routes
-  const spikeGet = (path: string, handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
-  const pdfHandlers = new PdfRoutes(licenceService)
-  spikeGet('/licence/id/:id/pdf/preview', pdfHandlers.renderPdf)
-  spikeGet('/licence/id/:id/preview', pdfHandlers.preview)
 
   return router
 }
