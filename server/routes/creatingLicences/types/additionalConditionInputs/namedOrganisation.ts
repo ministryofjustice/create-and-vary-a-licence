194e4b0120ae948bc32e774c50dde77d12b1f6f7
Stephen McVeigh <smcveigh941@gmail.com>
2021-11-11 10:52:43 +0000

    CVSL-224: Remove add another from conditions 9A and 9B

--- server/routes/creatingLicences/types/additionalConditionInputs/namedOrganisation.ts
+++ server/routes/creatingLicences/types/additionalConditionInputs/namedOrganisation.ts
@@ -1,10 +1,8 @@
 import { Expose } from 'class-transformer'
 import { ArrayNotEmpty } from 'class-validator'
-import RemoveEmptyArrayItems from '../../../../transformers/removeEmptyArrayItems'
 
 class NamedOrganisation {
   @Expose()
-  @RemoveEmptyArrayItems()
   @ArrayNotEmpty({ message: 'Add at least one group or organisation' })
   nameOfOrganisation: string[]
 }
--- server/routes/creatingLicences/types/additionalConditionInputs/specifiedItem.ts
+++ server/routes/creatingLicences/types/additionalConditionInputs/specifiedItem.ts
@@ -1,10 +1,8 @@
 import { Expose } from 'class-transformer'
 import { ArrayNotEmpty } from 'class-validator'
-import RemoveEmptyArrayItems from '../../../../transformers/removeEmptyArrayItems'
 
 class SpecifiedItem {
   @Expose()
-  @RemoveEmptyArrayItems()
   @ArrayNotEmpty({ message: 'Add at least one item' })
   item: string[]
 }
