6ea3785d54a4eb42c90b165d4ea438b42c45d277
Stephen McVeigh <smcveigh941@gmail.com>
2021-11-26 17:04:47 +0000

    CVSL-198: Display status badge on caseload view for case admin (#127)

--- server/routes/viewingLicences/handlers/viewLicence.ts
+++ server/routes/viewingLicences/handlers/viewLicence.ts
@@ -4,7 +4,12 @@ import LicenceStatus from '../../../enumeration/licenceStatus'
 export default class ViewAndPrintLicenceRoutes {
   GET = async (req: Request, res: Response): Promise<void> => {
     const { licence } = res.locals
-    if (licence?.statusCode === LicenceStatus.APPROVED || licence?.statusCode === LicenceStatus.ACTIVE) {
+    if (
+      licence?.statusCode === LicenceStatus.APPROVED ||
+      licence?.statusCode === LicenceStatus.ACTIVE ||
+      licence?.statusCode === LicenceStatus.SUBMITTED ||
+      licence?.statusCode === LicenceStatus.REJECTED
+    ) {
       res.render('pages/view/view')
     } else {
       res.redirect(`/licence/view/cases`)
