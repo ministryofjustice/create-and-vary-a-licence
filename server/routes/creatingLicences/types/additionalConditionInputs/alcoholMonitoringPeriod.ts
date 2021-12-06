e2906179db790870dd5218fc5ed8da827cd4302b
timharrison-moj <47779388+timharrison-moj@users.noreply.github.com>
2021-12-06 16:50:20 +0000

    CVSL-252: Added validation for date between CRD and LED. (#140)
    * CVSL-252: Added validation for date between CRD and LED.
    
    * CVSL-252: Added checks to alcohol monitring date as well.

--- server/routes/creatingLicences/types/additionalConditionInputs/alcoholMonitoringPeriod.ts
+++ server/routes/creatingLicences/types/additionalConditionInputs/alcoholMonitoringPeriod.ts
@@ -2,6 +2,8 @@ import { Expose, Type } from 'class-transformer'
 import { IsNotEmpty, Validate } from 'class-validator'
 import SimpleDate from '../date'
 import ValidSimpleDate from '../../../../validators/simpleDateValidator'
+import DateIsBefore from '../../../../validators/dateIsBefore'
+import DateIsAfter from '../../../../validators/dateIsAfter'
 
 class AlcoholMonitoringPeriod {
   @Expose()
@@ -11,6 +13,12 @@ class AlcoholMonitoringPeriod {
   @Expose()
   @Type(() => SimpleDate)
   @Validate(ValidSimpleDate)
+  @DateIsBefore('licence.licenceExpiryDate', {
+    message: 'The monitoring end date must be before the licence expiry date',
+  })
+  @DateIsAfter('licence.conditionalReleaseDate', {
+    message: 'The monitoring end date must be after the conditional release date',
+  })
   endDate: SimpleDate
 }
 
