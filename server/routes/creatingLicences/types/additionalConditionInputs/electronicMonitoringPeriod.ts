ca4952fc7703c84015772f0b75d3140506131373
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2021-12-06 16:23:31 +0000

    CVSL-252: Added validation for date between CRD and LED.

--- server/routes/creatingLicences/types/additionalConditionInputs/electronicMonitoringPeriod.ts
+++ server/routes/creatingLicences/types/additionalConditionInputs/electronicMonitoringPeriod.ts
@@ -2,11 +2,19 @@ import { Expose, Type } from 'class-transformer'
 import { Validate } from 'class-validator'
 import SimpleDate from '../date'
 import ValidSimpleDate from '../../../../validators/simpleDateValidator'
+import DateIsBefore from '../../../../validators/dateIsBefore'
+import DateIsAfter from '../../../../validators/dateIsAfter'
 
 class ElectronicMonitoringPeriod {
   @Expose()
   @Type(() => SimpleDate)
   @Validate(ValidSimpleDate)
+  @DateIsBefore('licence.licenceExpiryDate', {
+    message: 'The monitoring end date must be before the licence expiry date',
+  })
+  @DateIsAfter('licence.conditionalReleaseDate', {
+    message: 'The monitoring end date must be after the conditional release date',
+  })
   endDate: SimpleDate
 }
 
