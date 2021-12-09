c5a1933bedb96988c5444434ef6d5579ec082fa3
Stephen McVeigh <smcveigh941@gmail.com>
2021-12-09 12:27:51 +0000

    CVSL-137: Add edit question page

---
+++ server/routes/creatingLicences/handlers/editQuestion.test.ts
@@ -0,0 +1,78 @@
+import { Request, Response } from 'express'
+
+import EditQuestionRoutes from './editQuestion'
+import LicenceService from '../../../services/licenceService'
+import Mocked = jest.Mocked
+
+const licenceService = new LicenceService(null, null, null) as Mocked<LicenceService>
+
+describe('Route Handlers - Create Licence - Edit Licence Question', () => {
+  const handler = new EditQuestionRoutes(licenceService)
+  let req: Request
+  let res: Response
+
+  beforeEach(() => {
+    req = {
+      params: {
+        licenceId: '1',
+      },
+      user: {
+        username: 'joebloggs',
+      },
+    } as unknown as Request
+
+    res = {
+      render: jest.fn(),
+      redirect: jest.fn(),
+      locals: {
+        licence: {},
+        user: {
+          username: 'joebloggs',
+          displayName: 'Joe Bloggs',
+        },
+      },
+    } as unknown as Response
+
+    licenceService.updateStatus = jest.fn()
+  })
+
+  describe('GET', () => {
+    it('should redirect if not in the correct status', async () => {
+      res.locals.licence.statusCode = 'ACTIVE'
+      await handler.GET(req, res)
+      expect(res.redirect).toHaveBeenCalledWith(`/licence/create/id/1/check-your-answers`)
+    })
+
+    it('should render view', async () => {
+      res.locals.licence.statusCode = 'APPROVED'
+      await handler.GET(req, res)
+      expect(res.render).toHaveBeenCalledWith('pages/create/editQuestion')
+    })
+  })
+
+  describe('POST', () => {
+    it('should update status to IN_PROGRESS and redirect when answer is YES', async () => {
+      req = {
+        ...req,
+        body: {
+          answer: 'yes',
+        },
+      } as unknown as Request
+      await handler.POST(req, res)
+      expect(licenceService.updateStatus).toHaveBeenCalledWith('1', 'IN_PROGRESS', 'joebloggs', 'Joe Bloggs')
+      expect(res.redirect).toHaveBeenCalledWith('/licence/create/id/1/check-your-answers')
+    })
+
+    it('should not update status and should redirect when answer is NO', async () => {
+      req = {
+        ...req,
+        body: {
+          answer: 'no',
+        },
+      } as unknown as Request
+      await handler.POST(req, res)
+      expect(licenceService.updateStatus).not.toBeCalled()
+      expect(res.redirect).toHaveBeenCalledWith('/licence/create/id/1/check-your-answers')
+    })
+  })
+})
