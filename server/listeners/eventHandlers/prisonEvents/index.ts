beadd995c5729863c2c2370d01258ec6917462a5
Stephen McVeigh <smcveigh941@gmail.com>
2022-01-05 14:42:26 +0000

    CVSL-276: Add localstack to docker for local development

--- server/listeners/eventHandlers/prisonEvents/index.ts
+++ server/listeners/eventHandlers/prisonEvents/index.ts
@@ -3,7 +3,7 @@ import logger from '../../../../logger'
 
 export default async function handlePrisonEvents(messages: SQSMessage[]) {
   messages.forEach(message => {
-    const nomisEvent = JSON.parse(JSON.parse(message.Body).Message)
+    const nomisEvent = message.Body
     logger.info(`Nomis Event : ${nomisEvent}`)
 
     // TODO: Check event type here and call out to an event handler based on event type
