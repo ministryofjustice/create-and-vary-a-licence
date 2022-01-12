720fb64284727d7c020ee71dfc3c8fbebb654a06
Stephen McVeigh <smcveigh941@gmail.com>
2022-01-12 15:57:13 +0000

    CVSL-277: Log the event before parsing it

--- server/listeners/eventHandlers/prisonEvents/index.ts
+++ server/listeners/eventHandlers/prisonEvents/index.ts
@@ -5,8 +5,8 @@ import { PrisonEvent } from '../../../@types/prisonApiClientTypes'
 export default function buildEventHandler() {
   return async (messages: SQSMessage[]) => {
     messages.forEach(message => {
+      logger.info(`Nomis Event : ${message.Body}`)
       const nomisEvent = JSON.parse(JSON.parse(message.Body).Message) as PrisonEvent
-      logger.info(`Nomis Event : ${nomisEvent}`)
     })
   }
 }
--- server/listeners/eventHandlers/probationEvents/index.ts
+++ server/listeners/eventHandlers/probationEvents/index.ts
@@ -4,8 +4,7 @@ import logger from '../../../../logger'
 export default function buildEventHandler() {
   return async (messages: SQSMessage[]) => {
     messages.forEach(message => {
-      const deliusEvent = message.Body
-      logger.info(`Delius Event : ${deliusEvent}`)
+      logger.info(`Delius Event : ${message.Body}`)
 
       // TODO: Handle OFFENDER_CHANGED event here. Offender may have changed in a few ways
       //  (i.e. offender manager may have changed, or just something small like CRN has changed)
