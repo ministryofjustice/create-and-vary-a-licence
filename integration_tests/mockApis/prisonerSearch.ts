a408b5aed6cbd3d485d4e4471b1804a7bbadb418
Stephen Mc Veigh <smcveigh941@gmail.com>
2022-04-06 16:05:12 +0200

    CVSL-496: Integration test

--- integration_tests/mockApis/prisonerSearch.ts
+++ integration_tests/mockApis/prisonerSearch.ts
@@ -1,4 +1,5 @@
 import { SuperAgentRequest } from 'superagent'
+import moment from 'moment'
 import { stubFor } from '../wiremock'
 
 export default {
@@ -112,7 +113,7 @@ export default {
               receptionDate: '2021-01-08',
               locationDescription: 'Moorland (HMP & YOI)',
               restrictedPatient: false,
-              conditionalReleaseDate: '2022-09-09',
+              conditionalReleaseDate: moment().add(30, 'days').format('YYYY-MM-DD'),
             },
           ],
         },
