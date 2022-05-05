7ce2026b9f8dfb8f5ee300e1343c26bdc9e36b90
Stephen Mc Veigh <smcveigh941@gmail.com>
2022-05-05 18:48:25 +0200

    CVSL-517: Integration test

---
+++ integration_tests/pages/confirmCreate.ts
@@ -0,0 +1,26 @@
+import Page from './page'
+import AppointmentPlacePage from './appointmentPlace'
+import CheckAnswersPage from './checkAnswers'
+import AppointmentPersonPage from './appointmentPerson'
+
+export default class ConfirmCreatePage extends Page {
+  private yesRadioButtonId = '[value=Yes]'
+
+  private continueButtonId = '[data-qa=continue]'
+
+  constructor() {
+    super('confirm-create-page')
+  }
+
+  selectYes = (): ConfirmCreatePage => {
+    cy.get(this.yesRadioButtonId).click()
+    return this
+  }
+
+  clickContinue = (): AppointmentPersonPage => {
+    cy.task('stubGetStaffDetailsByStaffId')
+    cy.task('stubPostLicence')
+    cy.get(this.continueButtonId).click()
+    return Page.verifyOnPage(AppointmentPersonPage)
+  }
+}
