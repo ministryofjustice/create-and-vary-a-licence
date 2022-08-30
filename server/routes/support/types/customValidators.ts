5aa062b7d7aab008bb2263a419b4c19abad5e3c0
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2022-08-30 09:34:14 +0100

    CVSL-442, move date validation into middleware

--- server/routes/support/types/customValidators.ts
+++ server/routes/support/types/customValidators.ts
@@ -1,7 +1,11 @@
-import { registerDecorator, ValidationOptions } from 'class-validator'
+import { registerDecorator, ValidationOptions, ValidationArguments } from 'class-validator'
+
+interface DateAsString extends ValidationArguments {
+  object: { startDate: string; endDate: string }
+}
 
 export default class CustomValidators {
-  isDateFormat = (property: string, validationOptions?: ValidationOptions) => {
+  IsDateFormat = (property: string, validationOptions?: ValidationOptions) => {
     return (object: unknown, propertyName: string) => {
       registerDecorator({
         name: 'isDateFormat',
@@ -10,12 +14,45 @@ export default class CustomValidators {
         constraints: [property],
         options: validationOptions,
         validator: {
-          validate(value: string) {
+          validate(endDate: string) {
             const regex = /[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}/
-            return regex.test(value)
+            return regex.test(endDate)
           },
         },
       })
     }
   }
+
+  IsAfterStartDate = (property: string, validationOptions?: ValidationOptions) => {
+    return (object: unknown, propertyName: string) => {
+      registerDecorator({
+        name: 'isAfterStartDate',
+        target: object.constructor,
+        propertyName,
+        constraints: [property],
+        options: validationOptions,
+        validator: {
+          validate(_obj, DateRangeObj: DateAsString) {
+            return isStartBeforeEnd(DateRangeObj.object.startDate, DateRangeObj.object.endDate)
+          },
+        },
+      })
+    }
+  }
+}
+
+function isStartBeforeEnd(startDate: string, endDate: string) {
+  const parsedStartDate = startDate.split('/')
+  const parsedEndDate = endDate.split('/')
+  const StartDateDate = parseInt(parsedStartDate[0], 10)
+  const StartDateMonth = parseInt(parsedStartDate[1], 10)
+  const StartDateYear = parseInt(parsedStartDate[2], 10)
+
+  const EndDateDate = parseInt(parsedEndDate[0], 10)
+  const EndDateMonth = parseInt(parsedEndDate[1], 10)
+  const EndDateYear = parseInt(parsedEndDate[2], 10)
+
+  const dateStart = new Date(StartDateYear, StartDateMonth - 1, StartDateDate, 0, 0, 0, 0)
+  const dateEnd = new Date(EndDateYear, EndDateMonth - 1, EndDateDate + 1, 0, 0, 0, 0)
+  return dateStart < dateEnd
 }
--- server/routes/support/types/dateRange.ts
+++ server/routes/support/types/dateRange.ts
@@ -2,18 +2,17 @@ import { Expose } from 'class-transformer'
 import { IsNotEmpty } from 'class-validator'
 import CustomValidators from './customValidators'
 
-const { isDateFormat } = new CustomValidators()
+const { IsDateFormat, IsAfterStartDate } = new CustomValidators()
 
-class DateRange {
+export default class DateRange {
   @Expose()
   @IsNotEmpty({ message: 'Enter start date' })
-  @isDateFormat('startDate', { message: 'Invalid date format. Use d/m/yyyy' })
+  @IsDateFormat('startDate', { message: 'Invalid date format. Use d/m/yyyy' })
   startDate: string
 
   @Expose()
   @IsNotEmpty({ message: 'Enter end date' })
-  @isDateFormat('endDate', { message: 'Invalid date format. Use d/m/yyyy' })
+  @IsDateFormat('endDate', { message: 'Invalid date format. Use d/m/yyyy' })
+  @IsAfterStartDate('startDate', { message: 'End date must be on or after start date' })
   endDate: string
 }
-
-export default DateRange
