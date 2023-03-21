34569638e963a8327e5bff502cc2f78314e1b62b
sajid.khurshid <sajid.khurshid@digital.justice.gov.uk>
2023-03-21 08:27:32 +0000

    CVSL-889
    Patch update

--- server/validators/isValidExclusionZoneFile.ts
+++ server/validators/isValidExclusionZoneFile.ts
@@ -4,10 +4,14 @@ import { isBlank } from '../utils/utils'
 
 export default function IsValidExclusionZoneFile(validationOptions?: ValidationOptions) {
   const isValidExclusionZoneFile = (outOfBoundFilename: string, { object }: ValidationArguments) => {
-    const uploadFile = _.get(object, 'uploadFile')
+    const { uploadFile } = object as Record<string, Record<string, unknown>>
     // If there is a file upload present in the request then validate it
     if (uploadFile) {
-      return true
+      return (
+        uploadFile.fieldname === 'outOfBoundFilename' &&
+        uploadFile.originalname === outOfBoundFilename &&
+        uploadFile.mimetype === 'application/pdf'
+      )
     }
     // If no file upload present (amend on other fields) just check there is a file name already present
     return !isBlank(outOfBoundFilename)
