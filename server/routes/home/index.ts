3cbe440a3df224fba3c9bfda40ba39bd024a9d2b
sajid.khurshid <sajid.khurshid@digital.justice.gov.uk>
2023-03-31 15:20:06 +0100

    CVSL-890. Inital checkin.

--- server/routes/home/index.ts
+++ server/routes/home/index.ts
@@ -4,6 +4,7 @@ import HomeRoutes from './handlers/home'
 import AboutRoutes from './handlers/about'
 import ContactUsRoutes from './handlers/contactUs'
 import AccessibilityStatementRoutes from './handlers/accessibilityStatement'
+import config from '../../config'
 
 export default function Index(): Router {
   const router = Router()
@@ -14,6 +15,12 @@ export default function Index(): Router {
   const aboutHandler = new AboutRoutes()
   const contactUsHandler = new ContactUsRoutes()
   const accessibilityStatementHandler = new AccessibilityStatementRoutes()
+
+  if (config.serviceIsUnvailable) {
+    router.all('*', (req, res) => {
+      res.render('service-unavailable.njk')
+    })
+  }
 
   get('/', homeHandler.GET)
   get('/about', aboutHandler.GET)
