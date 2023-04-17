3b52802ce731a666295bb93d9968be5383284ed7
sajid.khurshid <sajid.khurshid@digital.justice.gov.uk>
2023-04-17 11:59:55 +0100

    review comments.

--- server/services/licenceService.ts
+++ server/services/licenceService.ts
@@ -639,7 +639,7 @@ export default class LicenceService {
   private async getCroNumberFromNomis(prisonerNumber: string, user: User): Promise<string> {
     const prisoners = await this.prisonerService.searchPrisonersByNomisIds([prisonerNumber], user)
     if (prisoners) {
-      if (prisoners[0]?.croNumber !== undefined) return prisoners[0]?.croNumber
+      if (prisoners[0]?.croNumber !== undefined) return prisoners[0]?.croNumber || ''
     }
     return ''
   }
