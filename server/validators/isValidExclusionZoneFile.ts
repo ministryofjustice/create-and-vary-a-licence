93fecb884c123649140ef1e617216a1e78ca6649
justin gardiner <justin.gardiner@digital.justice.gov.uk>
2023-04-27 13:00:22 +0100

    CVSL-863: Add max file size validation to exclusion zone map upload and update labels and existing validation messages. (#523)
    * CVSL-863: Add max file size validation to exclusion zone map upload and update labels and existing validation messages.
    
    * CVSL-863: Correct musts to must in policy label.

--- server/validators/isValidExclusionZoneFile.ts
+++ server/validators/isValidExclusionZoneFile.ts
@@ -2,6 +2,9 @@ import { registerDecorator, ValidationArguments, ValidationOptions } from 'class
 import { isBlank } from '../utils/utils'
 
 export default function IsValidExclusionZoneFile(validationOptions?: ValidationOptions) {
+  // Max exclusion file size in MB, this is configurable in licence API
+  const MAX_FILE_SIZE_MB = 10
+
   const isValidExclusionZoneFile = (outOfBoundFilename: string, { object }: ValidationArguments) => {
     const { uploadFile } = object as Record<string, Record<string, unknown>>
     // If there is a file upload present in the request then validate it
@@ -10,20 +13,36 @@ export default function IsValidExclusionZoneFile(validationOptions?: ValidationO
         uploadFile.fieldname === 'outOfBoundFilename' &&
         uploadFile.originalname === outOfBoundFilename &&
         uploadFile.mimetype === 'application/pdf' &&
-        (uploadFile.size as number) > 0
+        (uploadFile.size as number) > 0 &&
+        isValidSize(uploadFile)
       )
     }
     // If no file upload present (amend on other fields) just check there is a file name already present
     return !isBlank(outOfBoundFilename)
   }
 
-  return (object: unknown, propertyName: string) => {
+  const isValidSize = (uploadFile: Record<string, unknown>): boolean => {
+    const maxFileSizeInBytes = MAX_FILE_SIZE_MB * 1024 * 1024
+    const fileSize = uploadFile.size as number
+    return fileSize <= maxFileSizeInBytes
+  }
+
+  return (validatedObject: unknown, propertyName: string) => {
     registerDecorator({
       name: 'isValidExclusionZoneFile',
-      target: object.constructor,
+      target: validatedObject.constructor,
       propertyName,
       options: validationOptions,
-      validator: { validate: isValidExclusionZoneFile },
+      validator: {
+        validate: isValidExclusionZoneFile,
+        defaultMessage({ object }: ValidationArguments): string {
+          const { uploadFile } = object as Record<string, Record<string, unknown>>
+          if (uploadFile && !isValidSize(uploadFile)) {
+            return 'The selected file must be smaller than 10MB'
+          }
+          return 'Select a PDF map'
+        },
+      },
     })
   }
 }
