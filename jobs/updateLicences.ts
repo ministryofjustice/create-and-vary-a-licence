02ecc383c2b86cfef85c21a1c5302da1bd916415
Seb Negahban <seb.negahban@digital.justice.gov.uk>
2023-06-08 09:00:20 +0100

    Linting fixes

--- jobs/updateLicences.ts
+++ jobs/updateLicences.ts
@@ -1,15 +1,8 @@
 import 'reflect-metadata'
-import _ from 'lodash'
 import { initialiseAppInsights, buildAppInsightsClient, flush } from '../server/utils/azureAppInsights'
 import logger from '../logger'
 
-import { LicenceSummary } from '../server/@types/licenceApiClientTypes'
-import LicenceStatus from '../server/enumeration/licenceStatus'
 import LicenceApiClient from '../server/data/licenceApiClient'
-import PrisonApiClient from '../server/data/prisonApiClient'
-import PrisonerSearchApiClient from '../server/data/prisonerSearchApiClient'
-import { Prisoner } from '../server/@types/prisonerSearchApiClientTypes'
-import { isPassedArdOrCrd } from '../server/utils/utils'
 import { InMemoryTokenStore } from '../server/data/tokenStore'
 import { getSystemToken } from '../server/data/systemToken'
 
