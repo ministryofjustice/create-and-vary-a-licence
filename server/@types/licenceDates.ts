b48dfc3584b4bc55ccd11cbcdab9fdae2d996497
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2023-06-21 16:48:49 +0100

    add licence dates type missed from last commit.

---
+++ server/@types/licenceDates.ts
@@ -0,0 +1,10 @@
+export type LicenceDates = {
+  conditionalReleaseDate?: string
+  actualReleaseDate?: string
+  sentenceStartDate?: string
+  sentenceEndDate?: string
+  licenceStartDate?: string
+  licenceExpiryDate?: string
+  topupSupervisionStartDate?: string
+  topupSupervisionExpiryDate?: string
+}
