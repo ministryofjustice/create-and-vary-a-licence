4a658260521cd09434918a86625ca4ed413cd7ed
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2023-07-03 12:49:04 +0100

    CVSL-1145 Adding retries and fixing app insights on update job (#580)

--- server/data/systemToken.ts
+++ server/data/systemToken.ts
@@ -8,12 +8,12 @@ import config from '../config'
 export type SystemToken = { token: string; expiresIn: number }
 export type SystemTokenSupplier = (username?: string) => Promise<SystemToken>
 
-export const getSystemToken: SystemTokenSupplier = async (username?: string): Promise<SystemToken> => {
-  const clientToken = generateOauthClientToken(
-    config.apis.hmppsAuth.systemClientId,
-    config.apis.hmppsAuth.systemClientSecret
-  )
+const authHeaderValue = generateOauthClientToken(
+  config.apis.hmppsAuth.systemClientId,
+  config.apis.hmppsAuth.systemClientSecret
+)
 
+export const getSystemToken: SystemTokenSupplier = async (username?: string): Promise<SystemToken> => {
   const authRequest = username
     ? querystring.stringify({ grant_type: 'client_credentials', username })
     : querystring.stringify({ grant_type: 'client_credentials' })
@@ -24,9 +24,26 @@ export const getSystemToken: SystemTokenSupplier = async (username?: string): Pr
 
   const response = await superagent
     .post(`${config.apis.hmppsAuth.url}/oauth/token`)
-    .set('Authorization', clientToken)
+    .set('Authorization', authHeaderValue)
     .set('content-type', 'application/x-www-form-urlencoded')
     .send(authRequest)
+    .timeout(config.apis.hmppsAuth.timeout)
+
+  return { token: response.body.access_token, expiresIn: response.body.expires_in }
+}
+
+export const getSystemTokenWithRetries: SystemTokenSupplier = async (): Promise<SystemToken> => {
+  logger.info(`HMPPS Auth client creds request for client id '${config.apis.hmppsAuth.systemClientId}'`)
+
+  const response = await superagent
+    .post(`${config.apis.hmppsAuth.url}/oauth/token`)
+    .set('Authorization', authHeaderValue)
+    .set('content-type', 'application/x-www-form-urlencoded')
+    .retry(2, err => {
+      if (err) logger.info(`Retry handler for token call with ${err.code} ${err.message}`)
+      return undefined // retry handler only for logging retries, not to influence retry logic
+    })
+    .send(querystring.stringify({ grant_type: 'client_credentials' }))
     .timeout(config.apis.hmppsAuth.timeout)
 
   return { token: response.body.access_token, expiresIn: response.body.expires_in }
