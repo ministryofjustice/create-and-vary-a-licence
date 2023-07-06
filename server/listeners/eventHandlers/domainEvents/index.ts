9e585ad75f28de5ddbe4d003343d630d220be144
Seb Negahban <seb.negahban@digital.justice.gov.uk>
2023-07-06 17:51:54 +0100

    CVSL-830 Changing event source to be domain event

--- server/listeners/eventHandlers/domainEvents/index.ts
+++ server/listeners/eventHandlers/domainEvents/index.ts
@@ -4,17 +4,19 @@ import { Services } from '../../../services'
 import { DomainEventMessage } from '../../../@types/events'
 import ReleaseEventHandler from './releaseEventHandler'
 import TransferredEventHandler from './transferredEventHandler'
+import OffenderDetailsChangedEventHandler from './offenderDetailsChangedEventHandler'
 
 export default function buildEventHandler({ licenceService, prisonerService }: Services) {
   const releaseEventHandler = new ReleaseEventHandler(licenceService, prisonerService)
   const transferredEventHandler = new TransferredEventHandler(licenceService, prisonerService)
+  const offenderDetailsChangedEventHandler = new OffenderDetailsChangedEventHandler(licenceService, prisonerService)
 
   return async (messages: SQSMessage[]) => {
     messages.forEach(message => {
-      const prisonEvent = JSON.parse(message.Body)
+      const event = JSON.parse(message.Body)
 
-      const eventType = prisonEvent.MessageAttributes.eventType.Value
-      const eventMessage = JSON.parse(prisonEvent.Message) as DomainEventMessage
+      const eventType = event.MessageAttributes.eventType.Value
+      const eventMessage = JSON.parse(event.Message) as DomainEventMessage
 
       logger.info(`Domain Event (${eventType}) : ${JSON.stringify(eventMessage)}`)
 
@@ -24,6 +26,11 @@ export default function buildEventHandler({ licenceService, prisonerService }: S
           break
         case 'prison-offender-events.prisoner.received':
           transferredEventHandler.handle(eventMessage).catch(error => logger.error(error))
+          break
+        case 'prisoner-offender-search.prisoner.updated':
+          if (eventMessage.additionalInformation?.categoriesChanged?.includes('PERSONAL_DETAILS')) {
+            offenderDetailsChangedEventHandler.handle(eventMessage).catch(error => logger.error(error))
+          }
           break
       }
     })
--- server/listeners/eventHandlers/domainEvents/offenderDetailsChangedEventHandler.ts
+++ server/listeners/eventHandlers/domainEvents/offenderDetailsChangedEventHandler.ts
@@ -1,20 +1,17 @@
+import { DomainEventMessage } from '../../../@types/events'
 import LicenceService from '../../../services/licenceService'
 import PrisonerService from '../../../services/prisonerService'
 import { convertDateFormat, convertToTitleCase } from '../../../utils/utils'
-import { PrisonEventMessage } from '../../../@types/events'
 
 export default class OffenderDetailsChangedEventHandler {
   constructor(private readonly licenceService: LicenceService, private readonly prisonerService: PrisonerService) {}
 
-  handle = async (event: PrisonEventMessage) => {
-    const { bookingId, offenderIdDisplay } = event
-    const nomisId =
-      offenderIdDisplay ||
-      (await this.prisonerService.searchPrisonersByBookingIds([bookingId])).map(o => o.prisonerNumber).pop()
-    const prisoner = await this.prisonerService.getPrisonerDetail(nomisId)
+  handle = async (event: DomainEventMessage) => {
+    const { nomsNumber } = event.additionalInformation
+    const prisoner = await this.prisonerService.getPrisonerDetail(nomsNumber)
 
     if (prisoner) {
-      await this.licenceService.updateOffenderDetails(nomisId, {
+      await this.licenceService.updateOffenderDetails(nomsNumber, {
         forename: convertToTitleCase(prisoner.firstName),
         middleNames: convertToTitleCase(prisoner.middleName),
         surname: convertToTitleCase(prisoner.lastName),
