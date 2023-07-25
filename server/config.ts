dab4c16022331a9c0d3ab22e5f7fca0a58421f82
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2023-07-25 17:24:36 +0100

    CVSL-1230 Adding preprod phase banner

--- server/config.ts
+++ server/config.ts
@@ -32,6 +32,7 @@ export interface ApiConfig {
 export default {
   buildNumber: get('BUILD_NUMBER', '1_0_0', requiredInProduction),
   gitRef: get('GIT_REF', 'xxxxxxxxxxxxxxxxxxx', requiredInProduction),
+  phaseName: process.env.SYSTEM_PHASE || 'BETA',
   https: production,
   staticResourceCacheDuration: '1h',
   redis: {
