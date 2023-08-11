a44eb38ebd480ad0581b02cdd1800b0cb453073c
gurpreet.singh <gurpreet.singh@digital.justice.gov.uk>
2023-08-11 09:20:46 +0100

    CVSL-999 change search to a post form control

--- server/routes/search/handlers/probationSearch.ts
+++ server/routes/search/handlers/probationSearch.ts
@@ -4,10 +4,24 @@ import SearchService from '../../../services/searchService'
 export default class ProbationSearch {
   constructor(private readonly searchService: SearchService) {}
 
-  GET = async (req: Request, res: Response): Promise<void> => {
-    const { queryTerm } = req.query
-    const { staffCode } = req.query
+  POST = async (req: Request, res: Response): Promise<void> => {
+    const { queryTerm } = req.body
+    const { deliusStaffIdentifier } = res.locals.user
 
-    res.render('pages/search/probationSearch')
+    // need to call off to the search service to get back the results from the api
+    // the results back from the api may need to be put in a special model
+    // this means we can access the various parts of the object being sent back from the api
+    // once retrieved we will need to loop through the list of results and show the names etc based on the list
+    // the counts can be used as well
+
+    const inPrisonCount = 1
+    const onProbationCount = 0
+
+    res.render('pages/search/probationSearch', {
+      queryTerm,
+      deliusStaffIdentifier,
+      inPrisonCount,
+      onProbationCount,
+    })
   }
 }
--- server/routes/search/index.ts
+++ server/routes/search/index.ts
@@ -2,19 +2,22 @@ import { RequestHandler, Router } from 'express'
 import asyncMiddleware from '../../middleware/asyncMiddleware'
 import ProbationSearchRoutes from './handlers/probationSearch'
 import { Services } from '../../services'
+import roleCheckMiddleware from '../../middleware/roleCheckMiddleware'
+import validationMiddleware from '../../middleware/validationMiddleware'
 
 export default function Index({ searchService }: Services): Router {
   const router = Router()
 
   const routePrefix = (path: string) => `/search${path}`
 
-  const get = (path: string, handler: RequestHandler) => router.get(routePrefix(path), asyncMiddleware(handler))
+  const post = (path: string, handler: RequestHandler, type?: new () => object) =>
+    router.post(routePrefix(path), roleCheckMiddleware(['ROLE_LICENCE_RO']), asyncMiddleware(handler))
 
   // Handlers
   const probationSearchHandler = new ProbationSearchRoutes(searchService)
 
   // Operations
-  get('/probation-search', probationSearchHandler.GET)
+  post('/probation-search', probationSearchHandler.POST)
 
   return router
 }
