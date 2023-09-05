7185552d85b3d6031f881acb4f9fbfb79d23d4b9
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2023-09-05 10:45:02 +0100

    CVSL-1287 Allow for swapping input template based on condition (#652)
    Currently using same template for MEZ and normal conditions but will split out

--- server/utils/conditionRoutes.ts
+++ server/utils/conditionRoutes.ts
@@ -1,21 +1,38 @@
 export const MEZ_CONDITION_CODE = '0f9a20f4-35c7-4c77-8af8-f200f153fa11'
 
-type Args = {
+type EditConditionHrefArgs = {
   licenceId: number
   conditionId: number
   conditionCode: string
   fromReview: boolean
 }
 
-export const getEditConditionHref = ({ licenceId, conditionCode, conditionId, fromReview }: Args) => {
-  switch (conditionCode) {
-    case MEZ_CONDITION_CODE:
-      return `/licence/create/id/${licenceId}/additional-licence-conditions/condition/${conditionCode}/file-uploads${
-        fromReview ? '?fromReview=true' : ''
-      }`
-    default:
-      return `/licence/create/id/${licenceId}/additional-licence-conditions/condition/${conditionId}${
-        fromReview ? '?fromReview=true' : ''
-      }`
-  }
+type ConditionConfig = {
+  inputTemplate: string
+  getEditConditionHref: (args: EditConditionHrefArgs) => string
+}
+
+const DEFAULT_CONDITION_CONFIG: ConditionConfig = {
+  inputTemplate: 'pages/create/additionalLicenceConditionInput',
+  getEditConditionHref: (args: EditConditionHrefArgs) =>
+    `/licence/create/id/${args.licenceId}/additional-licence-conditions/condition/${args.conditionId}${
+      args.fromReview ? '?fromReview=true' : ''
+    }`,
+}
+
+const conditions: Record<string, ConditionConfig> = {
+  [MEZ_CONDITION_CODE]: {
+    inputTemplate: 'pages/create/additionalLicenceConditionInput',
+    getEditConditionHref: (args: EditConditionHrefArgs) =>
+      `/licence/create/id/${args.licenceId}/additional-licence-conditions/condition/${args.conditionCode}/file-uploads${
+        args.fromReview ? '?fromReview=true' : ''
+      }`,
+  },
+}
+
+export const getConfigForCondition = (conditionCode: string) => conditions[conditionCode] || DEFAULT_CONDITION_CONFIG
+
+export const getEditConditionHref = (args: EditConditionHrefArgs) => {
+  const config = getConfigForCondition(args.conditionCode)
+  return config.getEditConditionHref(args)
 }
