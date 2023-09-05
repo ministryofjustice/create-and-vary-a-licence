6f26ed897ff71d6726e1e09739e10ffdec4c6e09
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2023-09-05 07:59:30 +0100

    CVSL-1287 tidy up controllers (#648)
    * CVSL-1287 tidy up controllers
    
    * CVSL-1287 tidy up controllers
    
    ---------
    
    Co-authored-by: justin gardiner <justin.gardiner@digital.justice.gov.uk>

--- server/routes/manageConditions/index.ts
+++ server/routes/manageConditions/index.ts
@@ -5,34 +5,28 @@ import fetchLicence from '../../middleware/fetchLicenceMiddleware'
 import validationMiddleware from '../../middleware/validationMiddleware'
 import roleCheckMiddleware from '../../middleware/roleCheckMiddleware'
 
-import AdditionalLicenceConditionsQuestionRoutes from './handlers/additionalLicenceConditionsQuestion'
 import AdditionalLicenceConditionsRoutes from './handlers/additionalLicenceConditions'
-import BespokeConditionsQuestionRoutes from './handlers/bespokeConditionsQuestion'
 import BespokeConditionsRoutes from './handlers/bespokeConditions'
 import BespokeConditions from './types/bespokeConditions'
 
-import { Services } from '../../services'
+import type { Services } from '../../services'
 import AdditionalConditions from './types/additionalConditions'
 import AdditionalLicenceConditionsCallbackRoutes from './handlers/additionalLicenceConditionsCallback'
 import AdditionalLicenceConditionInputRoutes from './handlers/additionalLicenceConditionInput'
-import AdditionalPssConditionsQuestionRoutes from './handlers/additionalPssConditionsQuestion'
 import AdditionalPssConditionsRoutes from './handlers/additionalPssConditions'
 import AdditionalPssConditionsCallbackRoutes from './handlers/additionalPssConditionsCallback'
 import AdditionalPssConditionInputRoutes from './handlers/additionalPssConditionInput'
 import AdditionalLicenceConditionRemoveUploadRoutes from './handlers/additionalLicenceConditionRemoveUpload'
-import BespokeConditionsYesOrNo from './types/bespokeConditionsYesOrNo'
-import PssConditionsYesOrNo from './types/pssConditionsYesOrNo'
-import AdditionalConditionsYesOrNo from './types/additionalConditionsYesOrNo'
-import AdditionalLicenceConditionUploadsHandler from './handlers/additionalLicenceConditionUploadsHandler'
-import AdditionalLicenceTypesHandler from './handlers/additionalLicenceTypesHandler'
-import AdditionalLicenceConditionDeletionHandler from './handlers/additionalLicenceConditionDeletionHandler'
+import AdditionalLicenceConditionUploadsRoutes from './handlers/additionalLicenceConditionUploadsHandler'
+import AdditionalLicenceTypesRoutes from './handlers/additionalLicenceTypesHandler'
+import AdditionalLicenceConditionDeletionRoutes from './handlers/additionalLicenceConditionDeletionHandler'
 
 const upload = multer({ dest: 'uploads/' })
 
 export default function Index({ licenceService, conditionService }: Services): Router {
   const router = Router()
 
-  const routePrefix = (path: string) => `/licence/create${path}`
+  const routePrefix = (path: string) => `/licence/create/id/:licenceId${path}`
 
   /*
    * The fetchLicence middleware will call the licenceAPI during each GET request on the create a licence journey
@@ -67,87 +61,76 @@ export default function Index({ licenceService, conditionService }: Services): R
       asyncMiddleware(handler)
     )
 
-  const additionalLicenceConditionsQuestionHandler = new AdditionalLicenceConditionsQuestionRoutes()
-  const additionalLicenceConditionsHandler = new AdditionalLicenceConditionsRoutes(licenceService, conditionService)
-  const additionalLicenceConditionsCallbackHandler = new AdditionalLicenceConditionsCallbackRoutes(conditionService)
-  const additionalLicenceConditionInputHandler = new AdditionalLicenceConditionInputRoutes(
-    licenceService,
-    conditionService
-  )
-  const additionalLicenceConditionUploads = new AdditionalLicenceConditionUploadsHandler(
-    licenceService,
-    conditionService
-  )
-  const additionalLicenceTypesHandler = new AdditionalLicenceTypesHandler(licenceService)
-  const additionalLicenceConditionDeletionHandler = new AdditionalLicenceConditionDeletionHandler(licenceService)
-  const additionalLicenceConditionRemoveUploadHandler = new AdditionalLicenceConditionRemoveUploadRoutes(licenceService)
-  const additionalPssConditionsQuestionHandler = new AdditionalPssConditionsQuestionRoutes()
-  const additionalPssConditionsHandler = new AdditionalPssConditionsRoutes(licenceService, conditionService)
-  const additionalPssConditionsCallbackHandler = new AdditionalPssConditionsCallbackRoutes(conditionService)
-  const additionalPssConditionInputHandler = new AdditionalPssConditionInputRoutes(licenceService, conditionService)
-  const bespokeConditionsQuestionHandler = new BespokeConditionsQuestionRoutes()
-  const bespokeConditionsHandler = new BespokeConditionsRoutes(licenceService)
+  {
+    const controller = new AdditionalLicenceConditionsRoutes(licenceService, conditionService)
+    get('/additional-licence-conditions', controller.GET)
+    post('/additional-licence-conditions', controller.POST, AdditionalConditions)
+  }
 
-  get('/id/:licenceId/additional-licence-conditions-question', additionalLicenceConditionsQuestionHandler.GET)
-  post(
-    '/id/:licenceId/additional-licence-conditions-question',
-    additionalLicenceConditionsQuestionHandler.POST,
-    AdditionalConditionsYesOrNo
-  )
-  get('/id/:licenceId/additional-licence-conditions', additionalLicenceConditionsHandler.GET)
-  post('/id/:licenceId/additional-licence-conditions', additionalLicenceConditionsHandler.POST, AdditionalConditions)
-  get('/id/:licenceId/additional-licence-conditions/callback', additionalLicenceConditionsCallbackHandler.GET)
-  get('/id/:licenceId/additional-licence-conditions/condition/:conditionId', additionalLicenceConditionInputHandler.GET)
+  {
+    const controller = new AdditionalLicenceConditionsCallbackRoutes(conditionService)
+    get('/additional-licence-conditions/callback', controller.GET)
+  }
 
-  // Additional condition forms can include file uploads which uses `multer` middleware on these routes
-  postWithFileUpload(
-    '/id/:licenceId/additional-licence-conditions/condition/:conditionId',
-    additionalLicenceConditionInputHandler.POST
-  )
-  get('/id/:licenceId/condition/id/:conditionId/remove-upload', additionalLicenceConditionRemoveUploadHandler.GET)
-  get(
-    '/id/:licenceId/additional-licence-conditions/condition/:conditionCode/file-uploads',
-    additionalLicenceConditionUploads.GET
-  )
-  get(
-    '/id/:licenceId/additional-licence-conditions/condition/:conditionId/remove',
-    additionalLicenceConditionDeletionHandler.GET
-  )
-  post(
-    '/id/:licenceId/additional-licence-conditions/condition/:conditionId/remove',
-    additionalLicenceConditionDeletionHandler.POST
-  )
-  post(
-    '/id/:licenceId/additional-licence-conditions/condition/:conditionCode/file-uploads',
-    additionalLicenceConditionUploads.POST
-  )
+  // START MEZ
+  // remove area from map list in form builder without confirmation (delete single conditions) - DELETE NOT USED
+  {
+    const controller = new AdditionalLicenceConditionRemoveUploadRoutes(licenceService)
+    get('/condition/id/:conditionId/remove-upload', controller.GET)
+  }
 
-  get('/id/:licenceId/additional-licence-types/condition/:conditionCode/delete', additionalLicenceTypesHandler.DELETE)
+  {
+    // View map list / add new map condition
+    const controller = new AdditionalLicenceConditionUploadsRoutes(licenceService, conditionService)
+    get('/additional-licence-conditions/condition/:conditionCode/file-uploads', controller.GET)
+    post('/additional-licence-conditions/condition/:conditionCode/file-uploads', controller.POST)
+  }
 
-  get(
-    '/id/:licenceId/additional-licence-conditions/condition/:conditionId/delete',
-    additionalLicenceConditionInputHandler.DELETE
-  )
-  get('/id/:licenceId/additional-pss-conditions-question', additionalPssConditionsQuestionHandler.GET)
-  post(
-    '/id/:licenceId/additional-pss-conditions-question',
-    additionalPssConditionsQuestionHandler.POST,
-    PssConditionsYesOrNo
-  )
-  get('/id/:licenceId/additional-pss-conditions', additionalPssConditionsHandler.GET)
-  post('/id/:licenceId/additional-pss-conditions', additionalPssConditionsHandler.POST, AdditionalConditions)
-  get('/id/:licenceId/additional-pss-conditions/callback', additionalPssConditionsCallbackHandler.GET)
-  get('/id/:licenceId/additional-pss-conditions/condition/:conditionId', additionalPssConditionInputHandler.GET)
-  post('/id/:licenceId/additional-pss-conditions/condition/:conditionId', additionalPssConditionInputHandler.POST)
-  post(
-    '/id/:licenceId/additional-pss-conditions/condition/:conditionId/delete',
-    additionalPssConditionInputHandler.DELETE
-  )
-  get('/id/:licenceId/bespoke-conditions-question', bespokeConditionsQuestionHandler.GET)
-  post('/id/:licenceId/bespoke-conditions-question', bespokeConditionsQuestionHandler.POST, BespokeConditionsYesOrNo)
-  get('/id/:licenceId/bespoke-conditions', bespokeConditionsHandler.GET)
-  post('/id/:licenceId/bespoke-conditions', bespokeConditionsHandler.POST, BespokeConditions)
-  get('/id/:licenceId/bespoke-conditions/delete', bespokeConditionsHandler.DELETE)
+  // delete MEZ condition (delete all conditions)
+  {
+    const controller = new AdditionalLicenceTypesRoutes(licenceService)
+    get('/additional-licence-types/condition/:conditionCode/delete', controller.DELETE)
+  }
+
+  // remove area from map list with confirmation (delete single conditions)
+  {
+    const controller = new AdditionalLicenceConditionDeletionRoutes(licenceService)
+    get('/additional-licence-conditions/condition/:conditionId/remove', controller.GET)
+    post('/additional-licence-conditions/condition/:conditionId/remove', controller.POST)
+  }
+  // END MEZ
+
+  {
+    const controller = new AdditionalLicenceConditionInputRoutes(licenceService, conditionService)
+    get('/additional-licence-conditions/condition/:conditionId', controller.GET)
+    postWithFileUpload('/additional-licence-conditions/condition/:conditionId', controller.POST)
+    get('/additional-licence-conditions/condition/:conditionId/delete', controller.DELETE)
+  }
+
+  {
+    const controller = new AdditionalPssConditionsRoutes(licenceService, conditionService)
+    get('/additional-pss-conditions', controller.GET)
+    post('/additional-pss-conditions', controller.POST, AdditionalConditions)
+  }
+
+  {
+    const controller = new AdditionalPssConditionsCallbackRoutes(conditionService)
+    get('/additional-pss-conditions/callback', controller.GET)
+  }
+
+  {
+    const controller = new AdditionalPssConditionInputRoutes(licenceService, conditionService)
+    get('/additional-pss-conditions/condition/:conditionId', controller.GET)
+    post('/additional-pss-conditions/condition/:conditionId', controller.POST)
+    post('/additional-pss-conditions/condition/:conditionId/delete', controller.DELETE)
+  }
+
+  {
+    const controller = new BespokeConditionsRoutes(licenceService)
+    get('/bespoke-conditions', controller.GET)
+    post('/bespoke-conditions', controller.POST, BespokeConditions)
+    get('/bespoke-conditions/delete', controller.DELETE)
+  }
 
   return router
 }
