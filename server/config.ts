ca6388e1b885255975e1feb30ffe8ce13ecbff8d
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2023-09-29 07:13:30 +0100

    Update config.ts

--- server/config.ts
+++ server/config.ts
@@ -171,7 +171,7 @@ export default {
         response: Number(get('COMPONENT_API_TIMEOUT_SECONDS', 2000)),
         deadline: Number(get('COMPONENT_API_TIMEOUT_SECONDS', 2000)),
       },
-      agent: new AgentConfig(Number(get('COMPONENT_API_TIMEOUT_SECONDS', 10000))),
+      agent: new AgentConfig(Number(get('COMPONENT_API_TIMEOUT_SECONDS', 2000))),
       enabled: get('COMMON_COMPONENTS_ENABLED', 'true') === 'true',
     },
   },
