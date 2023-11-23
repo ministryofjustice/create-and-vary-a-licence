f62533561130bf8f13efcd6e99556f969c6499f6
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2023-11-23 15:28:31 +0000

    Revert "CVSL-1412 Adding a basic json representation of the team caseload (#744)"
    This reverts commit 6f115362146569349c7e4cb01b2dc8ffc24ca6cc.

--- server/routes/creatingLicences/handlers/caseload.ts
+++ server/routes/creatingLicences/handlers/caseload.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express'
+import moment from 'moment'
 import _ from 'lodash'
 import CaseloadService from '../../../services/caseloadService'
+import { convertToTitleCase } from '../../../utils/utils'
 import statusConfig from '../../../licences/licenceStatus'
+import LicenceStatus from '../../../enumeration/licenceStatus'
 import logger from '../../../../logger'
-import createCaseloadViewModel from '../../views/CaseloadViewModel'
 
 export default class CaseloadRoutes {
   constructor(private readonly caseloadService: CaseloadService) {}
@@ -15,6 +17,10 @@ export default class CaseloadRoutes {
     const { user } = res.locals
 
     logger.info(`GET caseload for ${user?.username} with roles ${user?.userRoles} team view: ${teamView}`)
+
+    const caseload = teamView
+      ? await this.caseloadService.getTeamCreateCaseload(user, req.session.teamSelection)
+      : await this.caseloadService.getStaffCreateCaseload(user)
 
     let teamName = null
     let multipleTeams = false
@@ -37,11 +43,41 @@ export default class CaseloadRoutes {
       req.session.returnToCase = '/licence/create/caseload'
     }
 
-    const caseload = teamView
-      ? await this.caseloadService.getTeamCreateCaseload(user, req.session.teamSelection)
-      : await this.caseloadService.getStaffCreateCaseload(user)
+    const caseloadViewModel = caseload
+      .map(c => {
+        const licence =
+          c.licences.length > 1 ? c.licences.find(l => l.status !== LicenceStatus.APPROVED) : _.head(c.licences)
 
-    const caseloadViewModel = createCaseloadViewModel(caseload, search)
+        return {
+          name: convertToTitleCase(`${c.nomisRecord.firstName} ${c.nomisRecord.lastName}`.trim()),
+          crnNumber: c.deliusRecord.offenderCrn,
+          prisonerNumber: c.nomisRecord.prisonerNumber,
+          releaseDate: moment(c.nomisRecord.releaseDate || c.nomisRecord.conditionalReleaseDate).format('DD MMM YYYY'),
+          licenceId: licence.id,
+          licenceStatus: licence.status,
+          licenceType: licence.type,
+          probationPractitioner: c.probationPractitioner,
+          isClickable:
+            c.probationPractitioner !== undefined &&
+            licence.status !== LicenceStatus.NOT_IN_PILOT &&
+            licence.status !== LicenceStatus.OOS_RECALL &&
+            licence.status !== LicenceStatus.OOS_BOTUS,
+        }
+      })
+      .filter(c => {
+        const searchString = search?.toLowerCase().trim()
+        if (!searchString) return true
+        return (
+          c.crnNumber?.toLowerCase().includes(searchString) ||
+          c.name.toLowerCase().includes(searchString) ||
+          c.probationPractitioner?.name.toLowerCase().includes(searchString)
+        )
+      })
+      .sort((a, b) => {
+        const crd1 = moment(a.releaseDate, 'DD MMM YYYY').unix()
+        const crd2 = moment(b.releaseDate, 'DD MMM YYYY').unix()
+        return crd1 - crd2
+      })
     res.render('pages/create/caseload', {
       caseload: caseloadViewModel,
       statusConfig,
