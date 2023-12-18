3ed71d3ffdbe989483abb498695e9d816ca9ddfa
Louise N <louise.norris@digital.justice.gov.uk>
2023-12-18 09:22:48 +0000

    CVSL-1448 hard stop licence created by prison lock screen for PP (#768)

---
+++ server/routes/creatingLicences/handlers/licenceCreatedByPrison.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'
+import LicenceService from '../../../services/licenceService'
+
+export default class PrisonWillCreateThisLicenceRoutes {
+  constructor(private readonly licenceService: LicenceService) {}
+
+  GET = async (req: Request, res: Response): Promise<void> => {
+    const { licence, user } = res.locals
+    const backLink = req.session.returnToCase || '/licence/create/caseload'
+    const { email } = await this.licenceService.getOmuEmail(licence.prisonCode, user)
+
+    return res.render('pages/create/licenceCreatedByPrison', {
+      licence,
+      omuEmail: email,
+      backLink,
+    })
+  }
+}
