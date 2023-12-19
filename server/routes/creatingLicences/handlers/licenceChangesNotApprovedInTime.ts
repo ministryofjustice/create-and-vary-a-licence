3adb1f1efe70c8e1d7432cbca22eac1c3ee09967
Louise N <louise.norris@digital.justice.gov.uk>
2023-12-19 14:28:44 +0000

    CVSL-1448 add licence not approved in time for release checks hard stop lock screen for PPs (#770)

---
+++ server/routes/creatingLicences/handlers/licenceChangesNotApprovedInTime.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express'
+import LicenceService from '../../../services/licenceService'
+import ConditionService from '../../../services/conditionService'
+import { groupingBy } from '../../../utils/utils'
+
+export default class LicenceChangesNotApprovedInTimeRoutes {
+  constructor(
+    private readonly licenceService: LicenceService,
+    private readonly conditionService: ConditionService
+  ) {}
+
+  GET = async (req: Request, res: Response): Promise<void> => {
+    const { licence, user } = res.locals
+    const backLink = req.session.returnToCase || '/licence/create/caseload'
+    const { email } = await this.licenceService.getOmuEmail(licence.prisonCode, user)
+
+    const conditionsToDisplay = await this.conditionService.getAdditionalAPConditionsForSummaryAndPdf(licence, user)
+
+    return res.render('pages/create/licenceChangesNotApprovedInTime', {
+      licence,
+      omuEmail: email,
+      additionalConditions: groupingBy(conditionsToDisplay, 'code'),
+      backLink,
+    })
+  }
+}
