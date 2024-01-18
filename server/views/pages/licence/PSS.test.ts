8814645de4e8a61d717028605152cca45fd3e1e0
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2024-01-18 12:57:54 +0000

    CVSL-1601 adding missing unit test

--- server/views/pages/licence/PSS.test.ts
+++ server/views/pages/licence/PSS.test.ts
@@ -1,23 +1,24 @@
 import fs from 'fs'
+
 import { templateRenderer } from '../../../utils/__testutils/templateTestUtils'
 import type { Licence } from '../../../@types/licenceApiClientTypes'
 
-const render = templateRenderer(fs.readFileSync('server/views/pages/licence/AP.njk').toString())
+const render = templateRenderer(fs.readFileSync('server/views/pages/licence/PSS.njk').toString())
 
-describe('Print an AP licence', () => {
-  it('verify render of an AP licence', () => {
+describe('Print a PSS licence', () => {
+  it('verify render of an PSS licence', () => {
     const $ = render({
       licence: {
         id: 1,
         forename: 'John',
         surname: 'Smith',
-        typeCode: 'AP',
+        typeCode: 'PSS' as const,
         version: '1.0',
         prisonCode: 'MDI',
         appointmentPerson: 'Jack Frost',
         appointmentAddress: 'The Square, Area, Town, County, S12 3QD',
         comTelephone: '07878 234566',
-        standardLicenceConditions: [
+        standardPssConditions: [
           { code: '1', text: 'Standard 1' },
           { code: '2', text: 'Standard 2' },
           { code: '3', text: 'Standard 3' },
@@ -26,7 +27,7 @@ describe('Print an AP licence', () => {
           { code: '6', text: 'Standard 6' },
           { code: '7', text: 'Standard 7' },
         ],
-        additionalLicenceConditions: [
+        additionalPssConditions: [
           {
             expandedText:
               'To comply with any requirements specified by your supervising officer for the purpose of ensuring that you address your drug and alcohold problems.',
@@ -49,64 +50,27 @@ describe('Print an AP licence', () => {
     expect($('title').text()).toContain('John Smith')
 
     // Check the licence header title contains the offender name
-    expect($('#title').text()).toContain('John Smith')
+    expect($('.title').text()).toContain('John Smith')
 
     // Check the offender image is present
-    expect($('#offender > #offender-image').length).toBe(1)
-
-    // Check the objectives section is present - 3 paragraphs, 1 bullet-point list
-    expect($('#objectives > p').length).toBe(3)
-    expect($('#objectives > .bullet-point').length).toBe(1)
-
-    // Check the supervision section is present with 2 bold dates
-    expect($('#ap-dates > p > .bold').length).toBe(2)
+    expect($('.offender > #offender-image').length).toBe(1)
 
     // Check the induction appointment is present with 3 paragraphs
     expect($('#induction > #meeting-details > p').length).toBe(3)
 
     // Check the release to other text is present
-    expect($('#released-to-other').text()).toContain('Criminal Justice Act 2003')
-
-    // Should be 7 standard, 1 additional and 1 bespoke conditions = 9 in total
-    expect($('#ap-conditions > .condition').length).toBe(9)
-
-    // Check the cancellation text is present
     expect($('#cancellation').text()).toContain('Criminal Justice Act 2003')
 
-    // Check the recall text is present
-    expect($('#recall').text()).toContain('Criminal Justice Act 2003')
+    // Should be 7 standard and 1 additional = 8 in total
+    expect($('#pss-conditions > .condition').length).toBe(8)
 
-    // Check the failure to comply text is present
-    expect($('#failure-to-comply').text()).toContain('licence revoked')
+    // Check the cancellation text is present
+    expect($('[data-qa="failure-to-comply"]').text()).toContain('If you fail to comply with any requirement')
 
     // Check the signature box and content are present
     expect($('.boxed > .signatures > p').length).toBe(6)
   })
 
-  it('SED and LED render together on one line if they are the same', () => {
-    const $ = render({
-      licence: {
-        licenceExpiryDate: '09/02/2022',
-        sentenceEndDate: '09/02/2022',
-      },
-    })
-
-    expect($('#offender > div > p:nth-child(2)').text().trim()).toBe(
-      'Your licence and sentence expire on 9th February 2022'
-    )
-  })
-
-  it('SED and LED render separately on different lines if they are different', () => {
-    const $ = render({
-      licence: {
-        licenceExpiryDate: '08/02/2022',
-        sentenceEndDate: '09/02/2022',
-      },
-    })
-
-    expect($('#offender > div > p:nth-child(2)').text().trim()).toBe('Your licence expires on 8th February 2022')
-    expect($('#offender > div > p:nth-child(3)').text().trim()).toBe('Your sentence expires on 9th February 2022')
-  })
   describe('Appointment date rendering', () => {
     it('Should render specific date time', () => {
       const $ = render({
