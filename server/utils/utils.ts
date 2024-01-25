ae8988744585f2821592d1b0c0f501ad601609dd
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2024-01-25 19:06:42 +0000

    date fixes

--- server/utils/utils.ts
+++ server/utils/utils.ts
@@ -196,7 +196,7 @@ const selectReleaseDate = (nomisRecord: Prisoner) => {
 }
 
 const isReleaseDateBeforeCutOffDate = (cutOffDate: string, releaseDate: string): boolean => {
-  return moment(new Date(releaseDate), 'DD/MM/YYYY').diff(moment(cutOffDate, 'DD/MM/YYYY')) <= 0
+  return moment(releaseDate, 'DD/MM/YYYY').diff(moment(cutOffDate, 'DD/MM/YYYY')) <= 0
 }
 
 const isPassedArdOrCrd = (licence: LicenceSummary, prisoner: Prisoner | PrisonApiPrisoner): boolean => {
