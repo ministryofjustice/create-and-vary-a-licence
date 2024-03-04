8b1baf6ad81ebca1d1650550c7612b8f408e626e
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2024-03-04 12:37:18 +0000

    Revert "CVSL-1618 implement activate vary licence reminder (#816)"
    This reverts commit 46834dc4e8674e8e859b48f8edb014e88bed2f2f.

--- server/routes/activateVaryLicenceReminderController/activateVaryLicenceReminderController.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express'
-
-export default class ActivateVaryLicenceReminderController {
-  GET = async (req: Request, res: Response): Promise<void> => {
-    res.render('pages/activateVaryLicenceReminderPage', {})
-  }
-}
