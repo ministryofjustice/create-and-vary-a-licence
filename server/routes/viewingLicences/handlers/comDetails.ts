78d304fc2c8e2360cd74a07862cd668fb0f2cf80
gurpreet.singh <gurpreet.singh@digital.justice.gov.uk>
2024-04-29 15:57:25 +0100

    CVSL-1851 add logic to change backlink button

--- server/routes/viewingLicences/handlers/comDetails.ts
+++ server/routes/viewingLicences/handlers/comDetails.ts
@@ -7,9 +7,10 @@ export default class ComDetailsRoutes {
   GET = async (req: Request, res: Response): Promise<void> => {
     const { staffCode } = req.params
     const staffDetails = await this.communityService.getStaffDetailByStaffCode(staffCode)
+    const activeTabToRedirect = req.query?.activeTab ? `#${req.query.activeTab}` : ''
 
     res.render('pages/comDetails', {
-      returnLink: '/licence/view/cases',
+      returnLink: `/licence/view/cases${activeTabToRedirect}`,
       name: `${staffDetails.staff?.forenames} ${staffDetails.staff?.surname}`.trim(),
       telephone: staffDetails.telephoneNumber,
       email: staffDetails.email,
