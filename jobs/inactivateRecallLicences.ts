6a4d24e3dbc8da32b7770c02b6f96ae03bd9af56
gurpreet.singh <gurpreet.singh@digital.justice.gov.uk>
2024-05-15 16:37:27 +0100

    CVSL-1738 add inactivate recall job to FE

---
+++ jobs/inactivateRecallLicences.ts
@@ -0,0 +1,28 @@
+/* eslint-disable import/first */
+/*
+ * Do appinsights first to instrument the logger
+ */
+import 'reflect-metadata'
+import { initialiseAppInsights, flush } from '../server/utils/azureAppInsights'
+import applicationInfo from '../server/applicationInfo'
+
+initialiseAppInsights(applicationInfo('create-and-vary-a-licence-inactivate-recall-licences'))
+
+import logger from '../logger'
+
+import LicenceApiClient from '../server/data/licenceApiClient'
+import { InMemoryTokenStore } from '../server/data/tokenStore'
+import { getSystemTokenWithRetries } from '../server/data/systemToken'
+
+const licenceApiClient = new LicenceApiClient(new InMemoryTokenStore(getSystemTokenWithRetries))
+
+licenceApiClient
+  .runInactivateRecallLicencesJob()
+  .then(() => {
+    // Flush logs to app insights and only exit when complete
+    flush({ callback: () => process.exit() }, 'success')
+  })
+  .catch((error: Error) => {
+    logger.error(error, 'Problem occurred while updating licence status to time out')
+    flush({ callback: () => process.exit(1) }, 'failure')
+  })
