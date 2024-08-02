dc381ea92865bd4da0d37adfd69134dae2198ba3
nishanthmahasamudram <129746957+nishanthmahasamudram@users.noreply.github.com>
2024-08-02 09:40:03 +0100

    Revert "Revert "Revert "Revert "CVSL-2044-CVL Caseload move CA caseload from …" (#1035)
    This reverts commit 73d88683408a364c07700a4bc25a64d5d9f43ea1.

--- server/routes/viewingLicences/handlers/viewCases.ts
+++ server/routes/viewingLicences/handlers/viewCases.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from 'express'
+import { format } from 'date-fns/format'
 import statusConfig from '../../../licences/licenceStatus'
 import { CaViewCasesTab } from '../../../utils/utils'
 import PrisonerService from '../../../services/prisonerService'
-import CaCaseloadService, { CaCase } from '../../../services/lists/caCaseloadService'
+import CaCaseloadService from '../../../services/lists/caCaseloadService'
 import LicenceStatus from '../../../enumeration/licenceStatus'
 import LicenceKind from '../../../enumeration/LicenceKind'
+import { CaCase } from '../../../@types/licenceApiClientTypes'
 
 export default class ViewAndPrintCaseRoutes {
   constructor(
@@ -43,16 +45,16 @@ export default class ViewAndPrintCaseRoutes {
     res.render('pages/view/cases', {
       cases: cases.map(c => {
         const link = this.getLink(c)
-        const licenceStatus = this.getStatus(c.licenceStatus)
+        const licenceStatus = this.getStatus(<LicenceStatus>c.licenceStatus)
         return {
           licenceId: c.licenceId,
           licenceVersionOf: c.licenceVersionOf,
           name: c.name,
           prisonerNumber: c.prisonerNumber,
           probationPractitioner: c.probationPractitioner,
-          releaseDate: c.releaseDate,
+          releaseDate: c.releaseDate ? format(c.releaseDate, 'dd MMM yyyy') : 'not found',
           releaseDateLabel: c.releaseDateLabel,
-          tabType: c.tabType,
+          tabType: CaViewCasesTab[c.tabType],
           nomisLegalStatus: c.nomisLegalStatus,
           lastWorkedOnBy: c.lastWorkedOnBy,
           isDueForEarlyRelease: c.isDueForEarlyRelease,
@@ -75,7 +77,14 @@ export default class ViewAndPrintCaseRoutes {
   }
 
   private getLink = (licence: CaCase): string => {
-    if (!this.isClickable(licence.kind, licence.licenceStatus, licence.isInHardStopPeriod, licence.tabType)) {
+    if (
+      !this.isClickable(
+        <LicenceKind>licence.kind,
+        <LicenceStatus>licence.licenceStatus,
+        licence.isInHardStopPeriod,
+        licence.tabType
+      )
+    ) {
       return null
     }
     if (licence.licenceStatus === LicenceStatus.TIMED_OUT) {
@@ -87,7 +96,8 @@ export default class ViewAndPrintCaseRoutes {
           ? `?lastApprovedVersion=${licence.licenceVersionOf}`
           : ''
 
-      return licence.isInHardStopPeriod && this.isEditableInHardStop(licence.kind, licence.licenceStatus)
+      return licence.isInHardStopPeriod &&
+        this.isEditableInHardStop(<LicenceKind>licence.kind, <LicenceStatus>licence.licenceStatus)
         ? `/licence/hard-stop/id/${licence.licenceId}/check-your-answers${query}`
         : `/licence/view/id/${licence.licenceId}/show${query}`
     }
@@ -99,9 +109,9 @@ export default class ViewAndPrintCaseRoutes {
     kind: LicenceKind,
     licenceStatus: LicenceStatus,
     isInHardStopPeriod: boolean,
-    tabType: CaViewCasesTab
+    tabType: string
   ): boolean => {
-    if (tabType === CaViewCasesTab.ATTENTION_NEEDED) {
+    if (tabType === 'attentionNeeded') {
       return false
     }
 
