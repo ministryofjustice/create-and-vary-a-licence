47840a10e047d6cc369bfb8984e9d6a8829452ca
Seb Negahban <102144566+SebNegahbanMoJ@users.noreply.github.com>
2024-09-17 14:49:35 +0100

    CVSL-1788 Adding routing for single instance file upload condition (#1066)

--- server/routes/manageConditions/handlers/fileUploads/fileUploadInputRoutes.test.ts
+++ server/routes/manageConditions/handlers/fileUploads/fileUploadInputRoutes.test.ts
@@ -9,7 +9,6 @@ jest.mock('../../../../services/licenceService')
 const licenceService = new LicenceService(null, null) as jest.Mocked<LicenceService>
 
 describe('Route Handlers - Create Licence - file upload input routes', () => {
-  const handler = new FileUploadInputRoutes(licenceService)
   let req: Request
   let res: Response
 
@@ -39,135 +38,273 @@ describe('Route Handlers - Create Licence - file upload input routes', () => {
     } as unknown as Response
   })
 
-  describe('POST', () => {
-    beforeEach(() => {
-      licenceService.updateAdditionalConditionData = jest.fn()
-      res.locals.licence = {
-        additionalLicenceConditions: [
-          {
-            id: 1,
-            code: 'code1',
+  describe('Multi-instance upload condition', () => {
+    const handler = new FileUploadInputRoutes(licenceService, true)
+    describe('POST', () => {
+      beforeEach(() => {
+        licenceService.updateAdditionalConditionData = jest.fn()
+        res.locals.licence = {
+          additionalLicenceConditions: [
+            {
+              id: 1,
+              code: 'code1',
+            },
+          ],
+        } as Licence
+      })
+
+      it('should call licence service to update the additional condition data', async () => {
+        await handler.POST(req, res)
+        expect(licenceService.updateAdditionalConditionData).toHaveBeenCalledWith(
+          '1',
+          { code: 'code1', id: 1 },
+          {},
+          { username: 'joebloggs' }
+        )
+      })
+
+      it('should redirect to the list view', async () => {
+        await handler.POST(req, res)
+        expect(res.redirect).toHaveBeenCalledWith(
+          '/licence/create/id/1/additional-licence-conditions/condition/code1/file-uploads'
+        )
+      })
+
+      it('should redirect to the callback function with query parameter if fromReview flag is true', async () => {
+        req.query.fromReview = 'true'
+        await handler.POST(req, res)
+        expect(res.redirect).toHaveBeenCalledWith(
+          '/licence/create/id/1/additional-licence-conditions/condition/code1/file-uploads?fromReview=true'
+        )
+      })
+    })
+
+    describe('POST with file upload', () => {
+      beforeEach(() => {
+        const uploadFile = {
+          path: 'test-file',
+          originalname: 'test.txt',
+          fieldname: 'outOfBoundFilename',
+          mimetype: 'application/pdf',
+          size: 100,
+        } as Express.Multer.File
+
+        req = {
+          params: {
+            licenceId: '1',
+            conditionId: '1',
           },
-        ],
-      } as Licence
+          file: uploadFile,
+          query: {},
+          body: { outOfBoundFilename: 'test.txt' },
+        } as unknown as Request
+
+        licenceService.uploadExclusionZoneFile = jest.fn()
+        licenceService.updateAdditionalConditionData = jest.fn()
+
+        res.locals.licence = {
+          additionalLicenceConditions: [
+            {
+              id: 1,
+              code: 'outOfBoundsRegion',
+              expandedText: 'expanded text',
+            },
+          ],
+        } as Licence
+      })
+
+      it('should recognise a file upload', async () => {
+        await handler.POST(req, res)
+        expect(licenceService.uploadExclusionZoneFile).toHaveBeenCalledWith('1', '1', req.file, {
+          username: 'joebloggs',
+        })
+        expect(licenceService.updateAdditionalConditionData).toHaveBeenCalledWith(
+          '1',
+          { code: 'outOfBoundsRegion', id: 1, expandedText: 'expanded text' },
+          { outOfBoundFilename: 'test.txt' },
+          { username: 'joebloggs' }
+        )
+      })
+
+      it('does not upload file if no file present on request', async () => {
+        const { file, ...reqWithoutFile } = req
+
+        await handler.POST(reqWithoutFile as Request, res)
+        expect(licenceService.uploadExclusionZoneFile).not.toHaveBeenCalledWith()
+        expect(licenceService.updateAdditionalConditionData).toHaveBeenCalledWith(
+          '1',
+          { code: 'outOfBoundsRegion', id: 1, expandedText: 'expanded text' },
+          { outOfBoundFilename: 'test.txt' },
+          { username: 'joebloggs' }
+        )
+      })
     })
 
-    it('should call licence service to update the additional condition data', async () => {
-      await handler.POST(req, res)
-      expect(licenceService.updateAdditionalConditionData).toHaveBeenCalledWith(
-        '1',
-        { code: 'code1', id: 1 },
-        {},
-        { username: 'joebloggs' }
-      )
-    })
+    describe('DELETE', () => {
+      beforeEach(() => {
+        licenceService.updateAdditionalConditions = jest.fn()
+        licenceService.deleteAdditionalCondition = jest.fn()
+        res.locals.licence = {
+          id: 1,
+          additionalLicenceConditions: [
+            {
+              id: 1,
+              code: 'code1',
+              expandedText: 'expanded text',
+            },
+            {
+              id: 2,
+              code: 'code2',
+              expandedText: 'more expanded text',
+            },
+          ],
+        } as Licence
+      })
 
-    it('should redirect to the list view', async () => {
-      await handler.POST(req, res)
-      expect(res.redirect).toHaveBeenCalledWith(
-        '/licence/create/id/1/additional-licence-conditions/condition/code1/file-uploads'
-      )
-    })
+      it('should call licence service to update the additional conditions with the condition removed', async () => {
+        await handler.DELETE(req, res)
+        expect(licenceService.deleteAdditionalCondition).toHaveBeenCalledWith(1, 1, { username: 'joebloggs' })
+      })
 
-    it('should redirect to the callback function with query parameter if fromReview flag is true', async () => {
-      req.query.fromReview = 'true'
-      await handler.POST(req, res)
-      expect(res.redirect).toHaveBeenCalledWith(
-        '/licence/create/id/1/additional-licence-conditions/condition/code1/file-uploads?fromReview=true'
-      )
+      it('should redirect to the area list view', async () => {
+        await handler.DELETE(req, res)
+        expect(res.redirect).toHaveBeenCalledWith(
+          '/licence/create/id/1/additional-licence-conditions/condition/code1/file-uploads'
+        )
+      })
     })
   })
 
-  describe('POST with file upload', () => {
-    beforeEach(() => {
-      const uploadFile = {
-        path: 'test-file',
-        originalname: 'test.txt',
-        fieldname: 'outOfBoundFilename',
-        mimetype: 'application/pdf',
-        size: 100,
-      } as Express.Multer.File
+  describe('Single-instance upload condition', () => {
+    const handler = new FileUploadInputRoutes(licenceService, false)
+    describe('POST', () => {
+      beforeEach(() => {
+        licenceService.updateAdditionalConditionData = jest.fn()
+        res.locals.licence = {
+          additionalLicenceConditions: [
+            {
+              id: 1,
+              code: 'code1',
+            },
+          ],
+        } as Licence
+      })
 
-      req = {
-        params: {
-          licenceId: '1',
-          conditionId: '1',
-        },
-        file: uploadFile,
-        query: {},
-        body: { outOfBoundFilename: 'test.txt' },
-      } as unknown as Request
+      it('should call licence service to update the additional condition data', async () => {
+        await handler.POST(req, res)
+        expect(licenceService.updateAdditionalConditionData).toHaveBeenCalledWith(
+          '1',
+          { code: 'code1', id: 1 },
+          {},
+          { username: 'joebloggs' }
+        )
+      })
 
-      licenceService.uploadExclusionZoneFile = jest.fn()
-      licenceService.updateAdditionalConditionData = jest.fn()
+      it('should redirect to the additional condition input callback', async () => {
+        await handler.POST(req, res)
+        expect(res.redirect).toHaveBeenCalledWith('/licence/create/id/1/additional-licence-conditions/callback')
+      })
 
-      res.locals.licence = {
-        additionalLicenceConditions: [
-          {
-            id: 1,
-            code: 'outOfBoundsRegion',
-            expandedText: 'expanded text',
+      it('should redirect to the callback function with query parameter if fromReview flag is true', async () => {
+        req.query.fromReview = 'true'
+        await handler.POST(req, res)
+        expect(res.redirect).toHaveBeenCalledWith(
+          '/licence/create/id/1/additional-licence-conditions/callback?fromReview=true'
+        )
+      })
+    })
+
+    describe('POST with file upload', () => {
+      beforeEach(() => {
+        const uploadFile = {
+          path: 'test-file',
+          originalname: 'test.txt',
+          fieldname: 'outOfBoundFilename',
+          mimetype: 'application/pdf',
+          size: 100,
+        } as Express.Multer.File
+
+        req = {
+          params: {
+            licenceId: '1',
+            conditionId: '1',
           },
-        ],
-      } as Licence
+          file: uploadFile,
+          query: {},
+          body: { outOfBoundFilename: 'test.txt' },
+        } as unknown as Request
+
+        licenceService.uploadExclusionZoneFile = jest.fn()
+        licenceService.updateAdditionalConditionData = jest.fn()
+
+        res.locals.licence = {
+          additionalLicenceConditions: [
+            {
+              id: 1,
+              code: 'outOfBoundsRegion',
+              expandedText: 'expanded text',
+            },
+          ],
+        } as Licence
+      })
+
+      it('should recognise a file upload', async () => {
+        await handler.POST(req, res)
+        expect(licenceService.uploadExclusionZoneFile).toHaveBeenCalledWith('1', '1', req.file, {
+          username: 'joebloggs',
+        })
+        expect(licenceService.updateAdditionalConditionData).toHaveBeenCalledWith(
+          '1',
+          { code: 'outOfBoundsRegion', id: 1, expandedText: 'expanded text' },
+          { outOfBoundFilename: 'test.txt' },
+          { username: 'joebloggs' }
+        )
+      })
+
+      it('does not upload file if no file present on request', async () => {
+        const { file, ...reqWithoutFile } = req
+
+        await handler.POST(reqWithoutFile as Request, res)
+        expect(licenceService.uploadExclusionZoneFile).not.toHaveBeenCalledWith()
+        expect(licenceService.updateAdditionalConditionData).toHaveBeenCalledWith(
+          '1',
+          { code: 'outOfBoundsRegion', id: 1, expandedText: 'expanded text' },
+          { outOfBoundFilename: 'test.txt' },
+          { username: 'joebloggs' }
+        )
+      })
     })
 
-    it('should recognise a file upload', async () => {
-      await handler.POST(req, res)
-      expect(licenceService.uploadExclusionZoneFile).toHaveBeenCalledWith('1', '1', req.file, { username: 'joebloggs' })
-      expect(licenceService.updateAdditionalConditionData).toHaveBeenCalledWith(
-        '1',
-        { code: 'outOfBoundsRegion', id: 1, expandedText: 'expanded text' },
-        { outOfBoundFilename: 'test.txt' },
-        { username: 'joebloggs' }
-      )
-    })
+    describe('DELETE', () => {
+      beforeEach(() => {
+        licenceService.updateAdditionalConditions = jest.fn()
+        licenceService.deleteAdditionalCondition = jest.fn()
+        res.locals.licence = {
+          id: 1,
+          additionalLicenceConditions: [
+            {
+              id: 1,
+              code: 'code1',
+              expandedText: 'expanded text',
+            },
+            {
+              id: 2,
+              code: 'code2',
+              expandedText: 'more expanded text',
+            },
+          ],
+        } as Licence
+      })
 
-    it('does not upload file if no file present on request', async () => {
-      const { file, ...reqWithoutFile } = req
+      it('should call licence service to update the additional conditions with the condition removed', async () => {
+        await handler.DELETE(req, res)
+        expect(licenceService.deleteAdditionalCondition).toHaveBeenCalledWith(1, 1, { username: 'joebloggs' })
+      })
 
-      await handler.POST(reqWithoutFile as Request, res)
-      expect(licenceService.uploadExclusionZoneFile).not.toHaveBeenCalledWith()
-      expect(licenceService.updateAdditionalConditionData).toHaveBeenCalledWith(
-        '1',
-        { code: 'outOfBoundsRegion', id: 1, expandedText: 'expanded text' },
-        { outOfBoundFilename: 'test.txt' },
-        { username: 'joebloggs' }
-      )
-    })
-  })
-
-  describe('DELETE', () => {
-    beforeEach(() => {
-      licenceService.updateAdditionalConditions = jest.fn()
-      licenceService.deleteAdditionalCondition = jest.fn()
-      res.locals.licence = {
-        id: 1,
-        additionalLicenceConditions: [
-          {
-            id: 1,
-            code: 'code1',
-            expandedText: 'expanded text',
-          },
-          {
-            id: 2,
-            code: 'code2',
-            expandedText: 'more expanded text',
-          },
-        ],
-      } as Licence
-    })
-
-    it('should call licence service to update the additional conditions with the condition removed', async () => {
-      await handler.DELETE(req, res)
-      expect(licenceService.deleteAdditionalCondition).toHaveBeenCalledWith(1, 1, { username: 'joebloggs' })
-    })
-
-    it('should redirect to the area list view', async () => {
-      await handler.DELETE(req, res)
-      expect(res.redirect).toHaveBeenCalledWith(
-        '/licence/create/id/1/additional-licence-conditions/condition/code1/file-uploads'
-      )
+      it('should redirect to the additional condition input callback', async () => {
+        await handler.DELETE(req, res)
+        expect(res.redirect).toHaveBeenCalledWith('/licence/create/id/1/additional-licence-conditions/callback')
+      })
     })
   })
 })
