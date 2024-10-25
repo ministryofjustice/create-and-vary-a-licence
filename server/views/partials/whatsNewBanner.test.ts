6fcc2626f8d8ec275691ad61492a093d5798c9b3
Seb Negahban <seb.negahban@digital.justice.gov.uk>
2024-10-25 17:02:59 +0100

    CVSL-2173 updating specs

--- server/views/partials/whatsNewBanner.test.ts
+++ server/views/partials/whatsNewBanner.test.ts
@@ -9,13 +9,13 @@ describe('Whats new banner', () => {
     const $ = render({
       showCommsBanner: true,
     })
-    expect($('body').text()).toContain('Licences for SDS40 releases')
+    expect($('body').text()).toContain('Updates to additional licence conditions')
   })
 
   it('Hide whats new banner if showCommsBanner is true', () => {
     const $ = render({
       showCommsBanner: false,
     })
-    expect($('body').text()).not.toContain('Licences for SDS40 releases')
+    expect($('body').text()).not.toContain('Updates to additional licence conditions')
   })
 })
