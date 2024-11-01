84cc58d713c950ce0188a052b76bdc92957e68e4
Seb Negahban <102144566+SebNegahbanMoJ@users.noreply.github.com>
2024-11-01 10:02:08 +0000

    Revert "CVSL-2052 Removing CRD validations in advance of SDS40 (#1018)" (#1097)
    This reverts commit 90ba86127452acf3d91023c64581e8d74bfc8248.

--- server/validators/dateIsAfterExpectedReleaseDate.ts
+++ server/validators/dateIsAfterExpectedReleaseDate.ts
@@ -1,25 +1,23 @@
 import { registerDecorator, ValidationArguments, ValidationOptions } from 'class-validator'
-// import moment from 'moment'
+import moment from 'moment'
 import _ from 'lodash'
 import type SimpleDate from '../routes/creatingLicences/types/date'
 import DateString from '../routes/creatingLicences/types/dateString'
 
 export default function DateIsAfterExpectedReleaseDate(validationOptions?: ValidationOptions) {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const DateIsAfterExpectedReleaseDate = async (date: SimpleDate | DateString, { object }: ValidationArguments) => {
-    return true
-    // const dateAsMoment = date.toMoment()
-    // const dateToCompare = moment(_.get(object, 'licence.earliestReleaseDate'), 'DD/MM/YYYY')
-    // if (!dateToCompare.isValid()) {
-    //   throw new Error(
-    //     `Date to compare is not in a valid date format: EarliestReleaseDate - ${_.get(
-    //       object,
-    //       'licence.earliestReleaseDate'
-    //     )}`
-    //   )
-    // }
+    const dateAsMoment = date.toMoment()
+    const dateToCompare = moment(_.get(object, 'licence.earliestReleaseDate'), 'DD/MM/YYYY')
+    if (!dateToCompare.isValid()) {
+      throw new Error(
+        `Date to compare is not in a valid date format: EarliestReleaseDate - ${_.get(
+          object,
+          'licence.earliestReleaseDate'
+        )}`
+      )
+    }
 
-    // return dateAsMoment.isSameOrAfter(dateToCompare)
+    return dateAsMoment.isSameOrAfter(dateToCompare)
   }
 
   return (object: unknown, propertyName: string) => {
