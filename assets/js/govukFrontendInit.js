46a9d8f03ded9de8822ba5bd0df9a4f42e6ca308
Louise Norris <louise.norris@digital.justice.gov.uk>
2024-11-21 18:03:37 +0000

    Revert "Merge branch 'CVSL-2241-integrate-dpr-component-library' of github.com:ministryofjustice/create-and-vary-a-licence into CVSL-2241-integrate-dpr-component-library"
    This reverts commit 6f38cfc6e457314958889352182160dbd23ce5e9, reversing
    changes made to 358ac9c4fcdc73ef981014d68b4713e26bf48dfb.

---
+++ assets/js/govukFrontendInit.js
@@ -0,0 +1,13 @@
+import { initAll } from '../../../../../assets/govuk/govuk-frontend.min.js'
+
+initAll()
+// Initiate the back links
+$('[class$=js-backlink]').on('click', e => {
+  e.preventDefault()
+  if ($('ul.govuk-error-summary__list').length > 0) {
+    window.history.go(-2)
+  } else {
+    window.history.go(-1)
+  }
+})
+
