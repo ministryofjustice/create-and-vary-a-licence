9b590493c54d2ceed3d4569ed5881ef012d8284f
Louise Norris <louise.norris@digital.justice.gov.uk>
2025-06-11 15:50:55 +0100

    CVSL-2241 add end of file empty lines

--- assets/js/govukInit.mjs
+++ assets/js/govukInit.mjs
@@ -3,11 +3,10 @@ import { initAll } from '/assets/govuk/all.js'
 initAll()
 // Initiate the back links
 $('[class$=js-backlink]').on('click', e => {
-    e.preventDefault()
-    if ($('ul.govuk-error-summary__list').length > 0) {
-      window.history.go(-2)
-    } else {
-      window.history.go(-1)
-    }
-  })
-  
+  e.preventDefault()
+  if ($('ul.govuk-error-summary__list').length > 0) {
+    window.history.go(-2)
+  } else {
+    window.history.go(-1)
+  }
+})
