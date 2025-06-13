f691dbc352294034583de118a6e08c388e3d286e
Gurps Singh <60613823+gurps-singh@users.noreply.github.com>
2025-06-13 12:19:45 +0100

    CVSL-2912 add tabs and empty states for CA search (#1238)

--- integration_tests/pages/caSearch.ts
+++ integration_tests/pages/caSearch.ts
@@ -3,11 +3,23 @@ import Page from './page'
 export default class SearchPage extends Page {
   private searchHeading = '#ca-search-heading'
 
+  private prisonTabTitle = '#tab-heading-prison'
+
+  private probationTabTitle = '#tab-heading-probation'
+
   constructor() {
     super('ca-search-page')
   }
 
   getSearchHeading = () => {
     return cy.get(this.searchHeading)
+  }
+
+  getPrisonTabTitle = () => {
+    return cy.get(this.prisonTabTitle)
+  }
+
+  getProbationTabTitle = () => {
+    return cy.get(this.probationTabTitle)
   }
 }
