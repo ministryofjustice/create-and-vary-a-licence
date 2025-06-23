e8b25b19df539fc135b2c697e5b1807377396ac9
Nishanth <129746957+nishanthmahasamudram@users.noreply.github.com>
2025-06-23 15:04:04 +0100

    Cvsl 2946 validation fix (#1249)
    * CVSL-2946-validation-fix
    
    * tests to prove
    
    * test fix
    
    * fix
    
    * test to prove
    
    * fix

--- server/routes/creatingLicences/types/licenceToSubmit.test.ts
+++ server/routes/creatingLicences/types/licenceToSubmit.test.ts
@@ -19,22 +19,6 @@ describe('@ValidateIf for electronicMonitoringProvider', () => {
     expect(errors.some(e => e.property === 'electronicMonitoringProvider')).toBe(false)
   })
 
-  it('should NOT validate electronicMonitoringProvider if property is null', async () => {
-    const plain = {
-      appointmentPersonType: 'DUTY_OFFICER',
-      appointmentAddress: 'address',
-      appointmentContact: '0123456789',
-      appointmentTimeType: 'IMMEDIATE_UPON_RELEASE',
-      additionalLicenceConditions: [] as string[],
-      additionalPssConditions: [] as string[],
-      electronicMonitoringProviderStatus: 'COMPLETE',
-      electronicMonitoringProvider: null as undefined,
-    }
-    const instance = plainToInstance(LicenceToSubmit, plain, { excludeExtraneousValues: true })
-    const errors = await validate(instance)
-    expect(errors.some(e => e.property === 'electronicMonitoringProvider')).toBe(false)
-  })
-
   it('should validate electronicMonitoringProvider if status is not NOT_NEEDED and property is present', async () => {
     const plain = {
       appointmentPersonType: 'DUTY_OFFICER',
