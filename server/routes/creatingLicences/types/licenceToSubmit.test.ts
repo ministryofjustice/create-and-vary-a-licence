1416411c37238f89d0690130b5793fd2c246a0c6
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2025-06-23 11:37:10 +0100

    tests to prove

--- server/routes/creatingLicences/types/licenceToSubmit.test.ts
+++ server/routes/creatingLicences/types/licenceToSubmit.test.ts
@@ -1,5 +1,42 @@
+<<<<<<< HEAD
 import { plainToInstance } from 'class-transformer'
 import LicenceToSubmit from './licenceToSubmit'
+=======
+import { Expose, plainToInstance, Type } from 'class-transformer'
+import { ValidateIf, ValidateNested } from 'class-validator'
+import LicenceToSubmit from './licenceToSubmit'
+import ElectronicMonitoringProvider from '../../manageConditions/types/electronicMonitoringProvider'
+
+// Test-only class without @Expose on electronicMonitoringProviderStatus
+class LicenceToSubmitNoExpose {
+  @Expose()
+  appointmentPersonType: string
+
+  @Expose()
+  appointmentAddress: string
+
+  @Expose()
+  appointmentContact: string
+
+  @Expose()
+  appointmentTimeType: string
+
+  @Expose()
+  additionalLicenceConditions: string[]
+
+  @Expose()
+  additionalPssConditions: string[]
+
+  // No @Expose() here!
+  electronicMonitoringProviderStatus: 'NOT_NEEDED' | 'NOT_STARTED' | 'COMPLETE'
+
+  @Expose()
+  @ValidateIf(o => o.electronicMonitoringProviderStatus !== 'NOT_NEEDED')
+  @ValidateNested()
+  @Type(() => Object)
+  electronicMonitoringProvider: ElectronicMonitoringProvider
+}
+>>>>>>> 965e3c34 (tests to prove)
 
 describe('LicenceToSubmit @Expose electronicMonitoringProviderStatus', () => {
   it('should include electronicMonitoringProviderStatus when @Expose is present', async () => {
@@ -30,7 +67,11 @@ describe('LicenceToSubmit @Expose electronicMonitoringProviderStatus', () => {
       electronicMonitoringProvider: undefined as undefined,
     }
 
+<<<<<<< HEAD
     const instance = plainToInstance(LicenceToSubmit, plain, { excludeExtraneousValues: true })
+=======
+    const instance = plainToInstance(LicenceToSubmitNoExpose, plain, { excludeExtraneousValues: true })
+>>>>>>> 965e3c34 (tests to prove)
     delete instance.electronicMonitoringProviderStatus
     expect(instance.electronicMonitoringProviderStatus).toBeUndefined()
   })
