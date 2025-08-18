0b29956be02f9996e86f850ec0ae192fcf99236d
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2025-08-18 22:16:08 +0100

    test fix

--- server/utils/nunjucksSetup.test.ts
+++ server/utils/nunjucksSetup.test.ts
@@ -703,8 +703,49 @@ describe('Nunjucks Filters', () => {
         actions: {
           items: [
             {
+              attributes: {
+                'data-qa': 'condition-action-COND456',
+              },
               href: `/licence/create/id/${licence.id}/additional-licence-conditions/condition/${condition.id}/delete?fromReview=true`,
               text: 'Delete',
+              visuallyHiddenText: 'Delete condition',
+            },
+          ],
+        },
+      })
+    })
+
+    it('returns a row with "Change" action when condition.data is empty', () => {
+      const licence = { id: 'LIC123' }
+      const condition = {
+        id: 'COND456',
+        code: 'CODE789',
+        category: 'Test Category',
+        sequence: 1,
+        data: [
+          {
+            id: 1,
+            sequence: 0,
+            field: 'probationRegion',
+            value: 'London',
+            contributesToLicence: true,
+          },
+        ],
+      }
+      const html = '<p>Condition HTML</p>'
+
+      expect(registerNunjucks().getGlobal('additionalConditionRow')(licence, condition, html, true)).toEqual({
+        sequence: 1,
+        key: { text: 'Test Category' },
+        value: { html: '<p>Condition HTML</p>' },
+        actions: {
+          items: [
+            {
+              attributes: {
+                'data-qa': 'condition-action-COND456',
+              },
+              href: '/licence/create/id/LIC123/additional-licence-conditions/condition/COND456?fromReview=true',
+              text: 'Change',
               visuallyHiddenText: 'Change condition',
             },
           ],
