506955a219e07d8426667d2853d96824794284f7
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2025-09-10 18:00:06 +0100

    CVSL-3311 Add temporary page for PRRD cases (#1363)

---
+++ server/routes/support/handlers/PrrdCasesByPrison.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express'
+import CaCaseloadService from '../../../services/lists/caCaseloadService'
+
+export default class PrrdCasesByPrisonRoutes {
+  constructor(private readonly caCaseloadService: CaCaseloadService) {}
+
+  GET = async (req: Request, res: Response): Promise<void> => {
+    const { user } = res.locals
+
+    const cases = await this.caCaseloadService.getPrisonOmuCaseload(
+      {
+        ...user,
+      },
+      [user.activeCaseload],
+    )
+
+    const caseload = cases
+      .filter(aCase => aCase.kind === 'PRRD')
+      .map(aCase => ({
+        ...aCase,
+        probationPractitioner: aCase.probationPractitioner.name,
+      }))
+    return res.render('pages/support/prrdCasesByPrison', { caseload, user })
+  }
+}
