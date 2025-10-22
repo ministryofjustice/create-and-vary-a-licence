a6e7d2684fa2338b514657951649c32b1b4bdf60
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2025-10-22 14:42:42 +0100

    CVSL-3366 Time-Served-PP-Show-Time-Served-cases-in-the-COM-caseload-my-cases-and-Team-cases-as-required

--- server/views/pages/vary/caseload.test.ts
+++ server/views/pages/vary/caseload.test.ts
@@ -165,4 +165,44 @@ describe('Caseload', () => {
     expect($('#release-date-1').text()).toBe('13 Feb 2023HDC release')
     expect($('.urgent-highlight-message').text().toString()).toEqual('HDC release')
   })
+
+  it('should highlight Not allocated yet label if licence has no probation practitioner and highlight a Time-served release warning label when licence is time-served', () => {
+    const $ = render({
+      caseload: [
+        {
+          licenceId: 3,
+          name: 'Test Person',
+          crnNumber: 'Z882661',
+          licenceType: 'AP',
+          releaseDate: '13 Feb 2023',
+          licenceStatus: 'REVIEW_NEEDED',
+          hardStopKind: 'TIME_SERVED',
+          probationPractitioner: null,
+          kind: 'TIME_SERVED',
+        },
+      ],
+      statusConfig: {
+        ACTIVE: {
+          label: 'Active',
+          description: 'Approved by the prison and is now the currently active licence',
+          colour: 'turquoise',
+        },
+        VARIATION_IN_PROGRESS: {
+          label: 'Variation in progress',
+          description: 'Variation in progress',
+          colour: 'blue',
+        },
+        REVIEW_NEEDED: {
+          label: 'Review needed',
+          description: 'Review needed',
+          colour: 'red',
+        },
+      },
+    })
+    expect($('tbody .govuk-table__row').length).toBe(1)
+    expect($('.status-badge').text().toString()).toContain('Review needed')
+    expect($('#release-date-1').text()).toBe('13 Feb 2023Time-served release')
+    expect($('.urgent-highlight-message').text().toString()).toEqual('Time-served release')
+    expect($('#probation-practitioner-1').text().toString()).toBe('Not allocated yet')
+  })
 })
