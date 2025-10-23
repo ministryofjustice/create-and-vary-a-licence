023386d4c146e074c8c585e76f06cb2114797165
Louise N <louise.norris@digital.justice.gov.uk>
2025-10-23 09:26:22 +0100

    CVSL-3379 vary approver case list sort and date format fix (#1463)
    * CVSL-3379 vary approver case list sort and date format fix
    
    * CVSL-3379 add missing release date formating to case list

--- server/services/lists/varyApproverCaseloadService.ts
+++ server/services/lists/varyApproverCaseloadService.ts
@@ -1,35 +1,21 @@
-import { format } from 'date-fns'
 import { User } from '../../@types/CvlUserDetails'
 import type { VaryApproverCase } from '../../@types/licenceApiClientTypes'
-import { parseCvlDate } from '../../utils/utils'
 import { LicenceApiClient } from '../../data'
 
 export default class VaryApproverCaseloadService {
   constructor(private readonly licenceApiClient: LicenceApiClient) {}
 
   async getVaryApproverCaseload(user: User, searchTerm: string): Promise<VaryApproverCase[]> {
-    return (
-      await this.licenceApiClient.getVaryApproverCaseload({
-        probationPduCodes: user.probationPduCodes,
-        searchTerm,
-      })
-    ).map(this.mapToCaseView)
+    return this.licenceApiClient.getVaryApproverCaseload({
+      probationPduCodes: user.probationPduCodes,
+      searchTerm,
+    })
   }
 
   async getVaryApproverCaseloadByRegion(user: User, searchTerm: string): Promise<VaryApproverCase[]> {
-    return (
-      await this.licenceApiClient.getVaryApproverCaseload({
-        probationAreaCode: user.probationAreaCode,
-        searchTerm,
-      })
-    ).map(this.mapToCaseView)
-  }
-
-  private mapToCaseView(aCase: VaryApproverCase) {
-    return {
-      ...aCase,
-      releaseDate: format(parseCvlDate(aCase.releaseDate), 'dd MMM yyyy'),
-      variationRequestDate: format(parseCvlDate(aCase.variationRequestDate), 'dd MMM yyyy'),
-    }
+    return this.licenceApiClient.getVaryApproverCaseload({
+      probationAreaCode: user.probationAreaCode,
+      searchTerm,
+    })
   }
 }
---
+++ server/views/pages/vary-approve/cases.test.ts
@@ -0,0 +1,111 @@
+import fs from 'fs'
+import { templateRenderer } from '../../../utils/__testutils/templateTestUtils'
+import LicenceType from '../../../enumeration/licenceType'
+
+const render = templateRenderer(fs.readFileSync('server/views/pages/vary-approve/cases.njk').toString())
+
+const pduCases = [
+  {
+    licenceId: 1,
+    name: 'Test Person One',
+    crnNumber: 'X12345',
+    licenceType: LicenceType.AP,
+    releaseDate: '01/05/2024',
+    variationRequestDate: '03/06/2024',
+    probationPractitioner: 'Com One',
+  },
+]
+
+const allRegionCases = [
+  {
+    licenceId: 1,
+    name: 'Test Person One',
+    crnNumber: 'X12345',
+    licenceType: LicenceType.AP,
+    releaseDate: '01/05/2024',
+    variationRequestDate: '03/06/2024',
+    probationPractitioner: 'Com One',
+  },
+  {
+    licenceId: 2,
+    name: 'Test Person Two',
+    crnNumber: 'X12346',
+    licenceType: LicenceType.AP,
+    releaseDate: '02/11/2022',
+    variationRequestDate: '01/08/2024',
+    probationPractitioner: 'Com Four',
+  },
+]
+
+describe('View Vary Approver case list', () => {
+  it('should display empty state correctly', () => {
+    const $ = render({
+      caseload: [],
+      search: '',
+      regionCases: false,
+    })
+    expect($('.govuk-heading-xl').text()).toBe('Select a person to approve their licence variation')
+    expect($('#myCases').text()).toContain('Cases in this PDU')
+    expect($('#allRegions').text()).toContain('All cases in this region')
+    expect($('#vary-approval-empty-state-content').text()).toContain(
+      `There are no licence variations which meet the search criteria.`,
+    )
+  })
+
+  it('should display pdu cases in a table with links to the licence and COM details page', () => {
+    const $ = render({
+      caseload: pduCases,
+      search: '',
+      regionCases: false,
+    })
+    expect($('.govuk-heading-xl').text()).toBe('Select a person to approve their licence variation')
+    expect($('#myCases').text()).toContain('Cases in this PDU')
+    expect($('#allRegions').text()).toContain('All cases in this region')
+    expect($('tbody .govuk-table__row').length).toBe(1)
+
+    expect($('#name-link-1').text()).toBe('Test Person One')
+    expect($('#name-link-1').attr('href').trim()).toBe('/licence/vary-approve/id/1/view')
+    expect($('#licence-type-1').text()).toContain('Standard determinate')
+    expect($('#probation-practitioner-1').text()).toBe('Com One')
+    expect($('#probation-practitioner-1 > .govuk-link').attr('href').trim()).toBe(
+      '/licence/vary-approve/id/1/probation-practitioner',
+    )
+
+    expect($('#variation-request-date-1').text()).toBe('3 Jun 2024')
+    expect($('#release-date-1').text()).toBe('1 May 2024')
+  })
+
+  it('should display all region cases in a table with links to the licence and COM details page', () => {
+    const $ = render({
+      caseload: allRegionCases,
+      search: '',
+      regionCases: true,
+    })
+    expect($('.govuk-heading-xl').text()).toBe('Select a person to approve their licence variation')
+    expect($('#myCases').text()).toContain('Cases in this PDU')
+    expect($('#allRegions').text()).toContain('All cases in this region')
+    expect($('tbody .govuk-table__row').length).toBe(2)
+
+    expect($('#name-link-1').text()).toBe('Test Person One')
+    expect($('#name-link-1').attr('href').trim()).toBe('/licence/vary-approve/id/1/view')
+    expect($('#licence-type-1').text()).toContain('Standard determinate')
+    expect($('#probation-practitioner-1').text()).toBe('Com One')
+    expect($('#probation-practitioner-1 > .govuk-link').attr('href').trim()).toBe(
+      '/licence/vary-approve/id/1/probation-practitioner',
+    )
+
+    expect($('#variation-request-date-1').text()).toBe('3 Jun 2024')
+    expect($('#release-date-1').text()).toBe('1 May 2024')
+
+    expect($('#name-link-2').text()).toBe('Test Person Two')
+    expect($('#name-link-2').attr('href').trim()).toBe('/licence/vary-approve/id/2/view')
+
+    expect($('#licence-type-2').text()).toContain('Standard determinate')
+    expect($('#probation-practitioner-2').text()).toBe('Com Four')
+    expect($('#probation-practitioner-2 > .govuk-link').attr('href').trim()).toBe(
+      '/licence/vary-approve/id/2/probation-practitioner',
+    )
+    expect($('#variation-request-date-2').text()).toBe('1 Aug 2024')
+    expect($('#release-date-2').text()).toBe('2 Nov 2022')
+  })
+})
