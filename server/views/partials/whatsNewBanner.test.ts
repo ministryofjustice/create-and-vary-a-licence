2762c8250544808d20d395db3a7b4a188da26014
Seb Negahban <102144566+SebNegahbanMoJ@users.noreply.github.com>
2025-10-28 10:00:59 +0000

    CVSL-3454 Updating What's New banner for recalls release (#1469)
    * CVSL-3454 Updating What's New banner for recalls release
    
    * CVSL-3454 enabling whats new banner in dev

--- server/views/partials/whatsNewBanner.test.ts
+++ server/views/partials/whatsNewBanner.test.ts
@@ -9,13 +9,35 @@ describe('Whats new banner', () => {
     const $ = render({
       showCommsBanner: true,
     })
-    expect($('body').text()).toContain('Updates to additional licence conditions')
+    expect($('body').text()).toContain("What's new")
   })
 
   it('Hide whats new banner if showCommsBanner is false', () => {
     const $ = render({
       showCommsBanner: false,
     })
-    expect($('body').text()).not.toContain('Updates to additional licence conditions')
+    expect($('body').text()).not.toContain("What's new")
+  })
+
+  it('Shows correct message for nomis user', () => {
+    const $ = render({
+      showCommsBanner: true,
+      user: {
+        authSource: 'nomis',
+      },
+    })
+    expect($('body').text()).toContain(
+      'If no licence has been submitted by 2 days before release, the prison can generate one.',
+    )
+  })
+
+  it('Shows correct message for delius user', () => {
+    const $ = render({
+      showCommsBanner: true,
+      user: {
+        authSource: 'delius',
+      },
+    })
+    expect($('body').text()).toContain('All licences must be submitted by 2 days before release.')
   })
 })
