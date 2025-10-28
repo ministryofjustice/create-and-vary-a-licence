6e9dee59cfadee981362d977a725a083af27a548
Gurps Singh <60613823+gurps-singh@users.noreply.github.com>
2025-10-28 16:21:37 +0000

    CVSL-3390 remove feature toggle for DPR work (#1472)

--- server/routes/support/handlers/supportHome.ts
+++ server/routes/support/handlers/supportHome.ts
@@ -1,9 +1,7 @@
 import { Request, Response } from 'express'
-import config from '../../../config'
 
 export default class SupportHomeRoutes {
   GET = async (req: Request, res: Response): Promise<void> => {
-    const { dprReportingEnabled } = config
-    res.render('pages/support/home', { dprReportingEnabled })
+    res.render('pages/support/home')
   }
 }
