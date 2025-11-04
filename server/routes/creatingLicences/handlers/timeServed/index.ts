798de7ce39aa1ae364b50b21783f6a073f35ffc1
Gurps Singh <60613823+gurps-singh@users.noreply.github.com>
2025-11-04 17:22:25 +0000

    CVSL-3407 add middleware check for hard stop on time served routes (#1490)

--- server/routes/creatingLicences/handlers/timeServed/index.ts
+++ server/routes/creatingLicences/handlers/timeServed/index.ts
@@ -8,14 +8,13 @@ import { Services } from '../../../../services'
 import ConfirmCreateRoutes from './confirmCreate'
 import CreateLicenceInNomisOrCvl from '../../types/createLicenceInNomisOrCvl'
 
-import config from '../../../../config'
+import hardStopCheckMiddleware from '../../../../middleware/hardStopCheckMiddleware'
+import UserType from '../../../../enumeration/userType'
 
 export default function Index({ licenceService, conditionService }: Services): Router {
   const router = Router()
 
   const routePrefix = (path: string) => `/licence/time-served${path}`
-
-  const { timeServedEnabled } = config
 
   /*
    * The fetchLicence middleware will call the licenceAPI during each GET request on the create a licence journey
@@ -24,7 +23,13 @@ export default function Index({ licenceService, conditionService }: Services): R
    * to explicitly inject the licence data into their individual view contexts.
    */
   const get = (path: string, handler: RequestHandler) =>
-    router.get(routePrefix(path), roleCheckMiddleware(['ROLE_LICENCE_CA']), fetchLicence(licenceService), handler)
+    router.get(
+      routePrefix(path),
+      roleCheckMiddleware(['ROLE_LICENCE_CA']),
+      fetchLicence(licenceService),
+      hardStopCheckMiddleware(UserType.PRISON),
+      handler,
+    )
 
   const post = (path: string, handler: RequestHandler, type?: new () => object) =>
     router.post(
@@ -32,19 +37,18 @@ export default function Index({ licenceService, conditionService }: Services): R
       roleCheckMiddleware(['ROLE_LICENCE_CA']),
       fetchLicence(licenceService),
       validationMiddleware(conditionService, type),
+      hardStopCheckMiddleware(UserType.PRISON),
       handler,
     )
   {
     const controller = new ConfirmCreateRoutes(licenceService)
 
-    if (timeServedEnabled) {
-      get('/create/nomisId/:nomisId/do-you-want-to-create-the-licence-on-this-service', controller.GET)
-      post(
-        '/create/nomisId/:nomisId/do-you-want-to-create-the-licence-on-this-service',
-        controller.POST,
-        CreateLicenceInNomisOrCvl,
-      )
-    }
+    get('/create/nomisId/:nomisId/do-you-want-to-create-the-licence-on-this-service', controller.GET)
+    post(
+      '/create/nomisId/:nomisId/do-you-want-to-create-the-licence-on-this-service',
+      controller.POST,
+      CreateLicenceInNomisOrCvl,
+    )
   }
   return router
 }
