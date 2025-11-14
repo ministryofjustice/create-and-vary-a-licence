f348621ae58c75b4ace207a6a7099c058fbe1499
justin gardiner <justin.gardiner@digital.justice.gov.uk>
2025-11-14 16:01:50 +0000

    CVSL-3513: Fix validation messages on the check your answers page. (#1503)

--- server/routes/creatingLicences/handlers/checkAnswers.ts
+++ server/routes/creatingLicences/handlers/checkAnswers.ts
@@ -102,7 +102,15 @@ export default class CheckAnswersRoutes {
   flattenValidationErrors = (errors: ValidationError[], parentProperty = ''): FieldValidationError[] =>
     errors.flatMap(error => {
       const propertyPath = parentProperty ? `${parentProperty}-${error.property}` : error.property
-      const current = error.constraints ? [{ field: propertyPath, message: Object.values(error.constraints)[0] }] : []
+      let current: FieldValidationError[] = []
+      if (error.constraints) {
+        const constraintKeys = Object.keys(error.constraints)
+        const lastConstraintKey = constraintKeys[constraintKeys.length - 1]
+        const message = error.constraints[lastConstraintKey]
+        const summaryMessage = error.contexts?.[lastConstraintKey]?.summaryMessageBuilder?.(message) || message
+
+        current = error.constraints ? [{ field: propertyPath, message, summaryMessage }] : []
+      }
       const children = error.children ? this.flattenValidationErrors(error.children, propertyPath) : []
       return [...current, ...children]
     })
