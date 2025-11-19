d597a74de9fed62e88abfa238bf7719d22883e13
Louise Norris <louise.norris@digital.justice.gov.uk>
2025-11-19 14:45:52 +0000

    CVSL-3409 add time served check middleware

--- server/middleware/timeServedCheckMiddleware.ts
+++ server/middleware/timeServedCheckMiddleware.ts
@@ -1,20 +1,19 @@
 import { RequestHandler } from 'express'
 import UserType from '../enumeration/userType'
-import { isInHardStopPeriod } from '../utils/utils'
 import logger from '../../logger'
 
-export default function hardStopCheckMiddleware(userType: UserType): RequestHandler {
+export default function timeServedCheckMiddleware(userType: UserType): RequestHandler {
   return async (req, res, next) => {
     const { licence } = res.locals
     // Prison should only be able to access initial appointment update pages in hard stop and time served, and probation should only be able to access
     // outside of hard stop and time served
 
-    if (!isInHardStopPeriod(licence) && userType === UserType.PRISON) {
-      logger.error('Access denied to prison user due to hard stop middleware')
+    if (licence.kind !== 'TIME_SERVED' && userType === UserType.PRISON) {
+      logger.error('Access denied to prison user due to time served middleware')
       return res.redirect('/access-denied')
     }
-    if (isInHardStopPeriod(licence) && userType === UserType.PROBATION) {
-      logger.error('Access denied to probation user due to hard stop middleware')
+    if (licence.kind === 'TIME_SERVED' && userType === UserType.PROBATION) {
+      logger.error('Access denied to probation user due to time served middleware')
       return res.redirect('/access-denied')
     }
 
--- server/routes/initialAppointment/handlers/timeServed/index.ts
+++ server/routes/initialAppointment/handlers/timeServed/index.ts
@@ -2,12 +2,14 @@ import { RequestHandler, Router } from 'express'
 import fetchLicence from '../../../../middleware/fetchLicenceMiddleware'
 import validationMiddleware from '../../../../middleware/validationMiddleware'
 import roleCheckMiddleware from '../../../../middleware/roleCheckMiddleware'
+import timeServedCheckMiddleware from '../../../../middleware/timeServedCheckMiddleware'
 
 import { Services } from '../../../../services'
 import PersonName from '../../types/personName'
 import InitialMeetingNameRoutes from './initialMeetingName'
 import InitialMeetingPlaceRoutes from './initialMeetingPlace'
 import PathType from '../../../../enumeration/pathType'
+import UserType from '../../../../enumeration/userType'
 
 export default function Index({ licenceService, conditionService, addressService }: Services): Router {
   const router = Router()
@@ -20,25 +22,32 @@ export default function Index({ licenceService, conditionService, addressService
    * This means that for each page, the licence will already exist in context, and so the handlers will not need
    * to explicitly inject the licence data into their individual view contexts.
    */
-  const get = (path: string, handler: RequestHandler) =>
-    router.get(routePrefix(path), roleCheckMiddleware(['ROLE_LICENCE_CA']), fetchLicence(licenceService), handler)
+  const get = (path: string, handler: RequestHandler, userType: UserType) =>
+    router.get(
+      routePrefix(path),
+      roleCheckMiddleware(['ROLE_LICENCE_CA']),
+      fetchLicence(licenceService),
+      timeServedCheckMiddleware(userType),
+      handler,
+    )
 
-  const post = (path: string, handler: RequestHandler, type?: new () => object) =>
+  const post = (path: string, handler: RequestHandler, userType: UserType, type?: new () => object) =>
     router.post(
       routePrefix(path),
       roleCheckMiddleware(['ROLE_LICENCE_CA']),
       fetchLicence(licenceService),
       validationMiddleware(conditionService, type),
+      timeServedCheckMiddleware(userType),
       handler,
     )
   {
     const controller = new InitialMeetingNameRoutes(licenceService, PathType.CREATE)
-    get('/create/id/:licenceId/initial-meeting-name', controller.GET)
-    post('/create/id/:licenceId/initial-meeting-name', controller.POST, PersonName)
+    get('/create/id/:licenceId/initial-meeting-name', controller.GET, UserType.PRISON)
+    post('/create/id/:licenceId/initial-meeting-name', controller.POST, UserType.PRISON, PersonName)
   }
   {
     const controller = new InitialMeetingPlaceRoutes(licenceService, addressService, PathType.CREATE)
-    get('/create/id/:licenceId/initial-meeting-place', controller.GET)
+    get('/create/id/:licenceId/initial-meeting-place', controller.GET, UserType.PRISON)
   }
   return router
 }
