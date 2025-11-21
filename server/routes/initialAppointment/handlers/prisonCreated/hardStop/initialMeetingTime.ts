1eb0beac15964cb30400ba09169064feb208bd9c
Louise Norris <louise.norris@digital.justice.gov.uk>
2025-11-21 17:01:19 +0000

    CVSL-3409 restructure prison created create and initial appointment routes to reflect new view layout

---
+++ server/routes/initialAppointment/handlers/prisonCreated/hardStop/initialMeetingTime.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express'
+import LicenceService from '../../../../../services/licenceService'
+import DateTime from '../../../types/dateTime'
+import UserType from '../../../../../enumeration/userType'
+import AppointmentTimeType from '../../../../../enumeration/appointmentTimeType'
+import flashInitialApptUpdatedMessage from '../../initialMeetingUpdatedFlashMessage'
+import PathType from '../../../../../enumeration/pathType'
+
+export default class InitialMeetingTimeRoutes {
+  constructor(
+    private readonly licenceService: LicenceService,
+    private readonly path: PathType,
+  ) {}
+
+  GET = async (req: Request, res: Response): Promise<void> => {
+    const { licence } = res.locals
+    const formDate = DateTime.toDateTime(licence.appointmentTime)
+    const appointmentTimeType: Record<string, string> = AppointmentTimeType
+
+    return res.render('pages/initialAppointment/prisonCreated/initialMeetingTime', {
+      formDate,
+      appointmentTimeType,
+      continueOrSaveLabel: this.path === PathType.EDIT ? 'Save' : 'Continue',
+    })
+  }
+
+  POST = async (req: Request, res: Response): Promise<void> => {
+    const { licenceId } = req.params
+    const { user, licence } = res.locals
+    await this.licenceService.updateAppointmentTime(licenceId, req.body, user)
+
+    flashInitialApptUpdatedMessage(req, licence, UserType.PRISON)
+
+    return res.redirect(`/licence/hard-stop/id/${licenceId}/check-your-answers`)
+  }
+}
