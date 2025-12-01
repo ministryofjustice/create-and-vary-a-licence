1542b738a1910c9f1ac4d5e841c0dd0aefbf7eec
AledWEvans <aled.evans1@justice.gov.uk>
2025-12-01 14:25:27 +0000

    CVSL-3565 CVL | Confirmation to review timeserved licence

--- server/views/pages/vary/reviewLicence.test.ts
+++ server/views/pages/vary/reviewLicence.test.ts
@@ -57,4 +57,29 @@ describe('Caseload', () => {
     expect(element.text()).toContain('This person was released following a fixed-term recall.')
     expect(element.text()).toContain('Check their previous licence conditions. You should do this using EPF 2.')
   })
+  it('should display appropriate time served hint message and labels', () => {
+    const $ = render({
+      licence: {
+        licenceId: 3,
+        name: 'Biydaav Griya',
+        crnNumber: 'Z882661',
+        typeCode: 'PSS',
+        releaseDate: '13 Feb 2023',
+        licenceStatus: 'ACTIVE',
+        probationPractitioner: { staffCode: 'X12342', name: 'CVL COM' },
+        postRecallReleaseDate: '2024-12-12',
+        eligibleKind: 'TIME_SERVED',
+        kind: 'TIME_SERVED',
+      },
+    })
+
+    const label = $(`label[for="radio-option"]`)
+    expect(label.text()).toContain('Yes - vary this licence now')
+
+    const label2 = $(`label[for="radio-option-2"]`)
+    expect(label2.text()).toContain('No - leave unchanged and return to licence history')
+
+    const message = $('[data-qa="additional-message"]')
+    expect(message.length).toBe(0)
+  })
 })
