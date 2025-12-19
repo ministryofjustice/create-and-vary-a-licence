c212b506ecefa67b2af4b63389985f6b198cb67c
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2025-12-19 12:55:01 +0000

    CVSL-3664: Fix app insights set up.

--- server.ts
+++ server.ts
@@ -1,15 +1,17 @@
+/* eslint-disable import/first */
 /*
- * Do AppInsights first as it does some magic instrumentation work, i.e. it affects other 'require's
+ * Do appinsights first as it does some magic instrumentation work, i.e. it affects other 'require's
  * In particular, applicationinsights automatically collects bunyan logs
  */
 import { initialiseAppInsights } from './server/utils/azureAppInsights'
 import createApplicationInfo from './server/applicationInfo'
-import logger from './logger'
-
-import { app, sqsDomainEventsListener, sqsPrisonEventsListener } from './server/index'
 
 const applicationInfo = createApplicationInfo()
 initialiseAppInsights(applicationInfo)
+
+import logger from './logger'
+
+import { app, sqsPrisonEventsListener, sqsDomainEventsListener } from './server/index'
 
 const server = app(applicationInfo)
 
