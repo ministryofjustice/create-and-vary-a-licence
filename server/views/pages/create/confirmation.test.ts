52d81ac320084b85d2ba504214158016794c15da
AledWEvans <aled.evans1@justice.gov.uk>
2026-03-02 16:59:28 +0000

    CVSL-3860 CVL | UI Remove some text on PP licence creation confirmation screen

--- server/views/pages/create/confirmation.test.ts
+++ server/views/pages/create/confirmation.test.ts
@@ -19,10 +19,6 @@ describe('Hdc Confirmation', () => {
     expect($('#message').text().trim().toString()).not.toContain(
       'You can do so up to 2 days before a standard release. From this point, you can only ask the prison to change the initial appointment details. Other changes must be made after release.',
     )
-
-    expect($('#message').text().trim().toString()).not.toContain(
-      'You can continue to work on licences for people being released early after the 2-day point.',
-    )
   })
 
   it('should show correct message when kind is not HDC', () => {
@@ -31,10 +27,6 @@ describe('Hdc Confirmation', () => {
     })
     expect($('#message').text().trim().toString()).toContain(
       'You can do so up to 2 days before a standard release. From this point, you can only ask the prison to change the initial appointment details. Other changes must be made after release.',
-    )
-
-    expect($('#continue-message').text().trim().toString()).toContain(
-      'You can continue to work on licences for people being released early after the 2-day point.',
     )
 
     expect($('#message').text()).not.toContain(
