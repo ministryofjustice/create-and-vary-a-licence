37ad76bea72ecb1ce4464425638fbde3614c1ba7
dave-virden <151018159+dave-virden@users.noreply.github.com>
2026-04-23 13:36:28 +0100

    CVSL-4016 Added banner for new standard recalls (#1741)
    * CVSL-4016 Added banner for new standard recalls
    
    * Lint
    
    ---------
    
    Co-authored-by: Seb Negahban <102144566+SebNegahbanMoJ@users.noreply.github.com>

--- server/views/partials/whatsNewBanner.test.ts
+++ server/views/partials/whatsNewBanner.test.ts
@@ -10,6 +10,24 @@ describe('Whats new banner', () => {
       showCommsBanner: true,
     })
     expect($('body').text()).toContain("What's new")
+    expect($('body').text()).toContain(
+      'From 28 April 2026, this service can be used to produce licences for people being released following a standard recall and PD1s will no longer need to be sent for these cases.',
+    )
+    expect($('body').text()).toContain(
+      'If no licence has been submitted by 2 days before release, the prison can generate one.',
+    )
+  })
+
+  it('Show whats new banner with probation staff message if isProbationStaff is true', () => {
+    const $ = render({
+      showCommsBanner: true,
+      isProbationStaff: true,
+    })
+    expect($('body').text()).toContain("What's new")
+    expect($('body').text()).toContain(
+      'From 28 April 2026, probation practitioners will be able to use this service to produce licences for people being released following a standard recall.',
+    )
+    expect($('body').text()).toContain('All licences must be submitted by 2 days before release.')
   })
 
   it('Hide whats new banner if showCommsBanner is false', () => {
