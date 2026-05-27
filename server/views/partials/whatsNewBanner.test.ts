766d597e75bc0becf2dfa023b81165628ede4d8f
dave-virden <151018159+dave-virden@users.noreply.github.com>
2026-05-27 16:21:08 +0100

    CVSL-4162 Added restricted patients banner and enabled in dev

--- server/views/partials/whatsNewBanner.test.ts
+++ server/views/partials/whatsNewBanner.test.ts
@@ -5,14 +5,20 @@ import { templateRenderer } from '../../utils/__testutils/templateTestUtils'
 const render = templateRenderer(fs.readFileSync('server/views/partials/whatsNewBanner.njk').toString())
 
 describe('Whats new banner', () => {
-  it('Show whats new banner if showCommsBanner is true', () => {})
-
-  it('Show whats new banner with probation staff message if isProbationStaff is true', () => {})
+  it('Show whats new banner if showCommsBanner is true', () => {
+    const $ = render({
+      showCommsBanner: true,
+    })
+    expect($('body').text()).toContain(`What's new`)
+    expect($('body').text()).toContain(
+      'From 1 June 2026, licences for people who are detained in a secure hospital can be created and managed using this service.',
+    )
+  })
 
   it('Hide whats new banner if showCommsBanner is false', () => {
     const $ = render({
       showCommsBanner: false,
     })
-    expect($('body').text()).not.toContain("What's new")
+    expect($('body').text()).not.toContain(`What's new`)
   })
 })
