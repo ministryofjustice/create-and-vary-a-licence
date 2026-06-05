630608e675422a2189e047df160c84fc9a1a5f94
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2026-06-05 13:09:28 +0100

    CVSL-3948: Fix add another and disable hidden inputs both creating their own onload event, instead add to the event listeners.

--- assets/js/conditions/disableHiddenConditionalInputs.js
+++ assets/js/conditions/disableHiddenConditionalInputs.js
@@ -1,11 +1,11 @@
-window.onload = function () {
+window.addEventListener('load', function () {
   function disableHiddenInputs() {
-    const hiddenInputs = document.querySelectorAll('.govuk-radios__conditional--hidden input, .govuk-radios__conditional--hidden select')
-    for (elem of hiddenInputs) {
-      elem.disabled = true
-    }
+      const hiddenInputs = document.querySelectorAll('.govuk-radios__conditional--hidden input, .govuk-radios__conditional--hidden select')
+      for (elem of hiddenInputs) {
+        elem.disabled = true
+      }
   }
 
   const form = document.querySelector('#additionConditionInputs')
   form.addEventListener('submit', disableHiddenInputs)
-}
+})
