8cfa47c09d3ce1d4d247e03ee6b5d0b31fc4d7fa
Nishanth <129746957+nishanthmahasamudram@users.noreply.github.com>
2026-06-15 09:24:52 +0100

    CVSL-4123 Update Variation Reason page for COM (#1808)

--- integration_tests/pages/reasonForVariationPage.ts
+++ integration_tests/pages/reasonForVariationPage.ts
@@ -10,6 +10,13 @@ export default class ReasonForVariationPage extends Page {
     super('reason-for-variation-page')
   }
 
+  verifyGuidanceText = (): ReasonForVariationPage => {
+    cy.contains('Add a short explanation for everything you want to vary').should('be.visible')
+    cy.contains("why the changes are being made now and why they're necessary to manage risk").should('be.visible')
+    cy.contains('who you have consulted about the variations and if they support your decision').should('be.visible')
+    return this
+  }
+
   enterReason = (text: string): ReasonForVariationPage => {
     cy.get(this.reasonTextArea).type(text, { force: true })
     return this
