a8b7a4061d6211573fd141abd0774c4835239f1e
nishanthmahasamudram <nishanth.mahasamudram@justice.gov.uk>
2026-06-18 08:28:51 +0100

    CVSL-4183 Vary Do the same HDC curfew hours apply every day page

---
+++ server/routes/varyingLicences/hdc/handlers/doHdcCurfewHoursApplyDaily.test.ts
@@ -0,0 +1,69 @@
+import fs from 'fs'
+import { Request, Response } from 'express'
+import DoHdcCurfewHoursApplyDailyRoutes from './doHdcCurfewHoursApplyDaily'
+import { templateRenderer } from '../../../../utils/__testutils/templateTestUtils'
+
+const render = templateRenderer(fs.readFileSync('server/views/pages/hdc/doHdcCurfewHoursApplyDaily.njk').toString())
+
+describe('Route Handlers - Vary Licence - Do HDC Curfew Hours Apply Daily', () => {
+  let req: Request
+  let res: Response
+
+  beforeEach(() => {
+    req = {
+      params: {
+        licenceId: 1,
+      },
+      body: {},
+      query: {},
+    } as unknown as Request
+
+    res = {
+      render: jest.fn(),
+      redirect: jest.fn(),
+      locals: {
+        user: {
+          username: 'joebloggs',
+        },
+        licence: {},
+      },
+    } as unknown as Response
+  })
+
+  const handler = new DoHdcCurfewHoursApplyDailyRoutes()
+
+  describe('GET', () => {
+    it('should render view', async () => {
+      req.query = { edit: 'telNumber' }
+      await handler.GET(req, res)
+      expect(res.render).toHaveBeenCalledWith('pages/hdc/doHdcCurfewHoursApplyDaily')
+    })
+  })
+
+  describe('View', () => {
+    it('should display the question', () => {
+      const $ = render({})
+      expect($('legend').text().trim()).toBe('Do the same HDC curfew hours apply every day?')
+    })
+  })
+
+  describe('POST', () => {
+    it('should redirect to the individual curfew hours page', async () => {
+      await handler.POST(req, res)
+      expect(res.redirect).toHaveBeenCalledWith('/licence/vary/id/1/hdc/individual-curfew-hours')
+    })
+
+    it('should redirect to the curfew hours page', async () => {
+      req.body = { answer: 'Yes' }
+      await handler.POST(req, res)
+      expect(res.redirect).toHaveBeenCalledWith('/licence/vary/id/1/hdc/curfew-hours')
+    })
+
+    it('should redirect to the curfew hours page with fromReview query param', async () => {
+      req.body = { answer: 'Yes' }
+      req.query = { fromReview: 'true' }
+      await handler.POST(req, res)
+      expect(res.redirect).toHaveBeenCalledWith('/licence/vary/id/1/hdc/curfew-hours?fromReview=true')
+    })
+  })
+})
---
+++ server/routes/varyingLicences/hdc/handlers/doHdcCurfewHoursApplyDaily.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express'
+
+export default class DoHdcCurfewHoursApplyDailyRoutes {
+  GET = async (req: Request, res: Response): Promise<void> => res.render('pages/hdc/doHdcCurfewHoursApplyDaily')
+
+  POST = async (req: Request, res: Response): Promise<void> => {
+    const { licenceId } = req.params
+    const { answer } = req.body
+    const nextPath = answer === 'Yes' ? 'curfew-hours' : 'individual-curfew-hours'
+
+    const redirectPath = `/licence/vary/id/${licenceId}/hdc/${nextPath}${
+      req.query?.fromReview ? '?fromReview=true' : ''
+    }`
+
+    res.redirect(redirectPath)
+  }
+}
