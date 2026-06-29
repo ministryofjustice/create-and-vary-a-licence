067fa3f9789db367d015d8b7f9d0cd11124079ec
nishanthmahasamudram <nishanth.mahasamudram@justice.gov.uk>
2026-06-29 07:55:15 +0100

    CVSL-4188 Add HDC details to PDU head reason for declining page

--- integration_tests/integration/hdc/approveVariation.cy.ts
+++ integration_tests/integration/hdc/approveVariation.cy.ts
@@ -2,7 +2,7 @@ import Page from '../../pages/page'
 import IndexPage from '../../pages'
 import LicenceKind from '../../../server/enumeration/LicenceKind'
 
-context('ACO review a licence variation', () => {
+context('ACO review a HDC licence variation', () => {
   const curfewAddress = {
     firstLine: '123 Fake Street',
     secondLine: 'Apt 4',
@@ -37,7 +37,7 @@ context('ACO review a licence variation', () => {
     cy.signIn()
   })
 
-  it('ACO approve a licence variation', () => {
+  it('ACO approve a HDC licence variation', () => {
     cy.task('stubApproveVariation')
     cy.task('stubGetHdcLicence', {
       licenceId: '120',
@@ -58,6 +58,33 @@ context('ACO review a licence variation', () => {
     varyApproveViewPage.checkHdcCurfewDetails('123 Fake Street, Apt 4, Fakecounty, FK1 2AB', true)
     const varyApproveConfirmPage = varyApproveViewPage.clickApproveVariation()
     varyApproveCasesPage = varyApproveConfirmPage.clickBackToCaseList()
+    varyApproveCasesPage.signOut().click()
+  })
+
+  it('ACO reject a HDC licence variation', () => {
+    cy.task('stubReferVariation')
+    cy.task('stubGetHdcLicence', {
+      licenceId: '120',
+      licenceKind: LicenceKind.HDC,
+      curfewAddress: { ...curfewAddress, secondLine: 'Apt 5' },
+      weeklyCurfewTimes: [
+        {
+          curfewTimesSequence: 0,
+          fromTime: '20:00:00',
+          untilTime: '06:00:00',
+        },
+      ],
+    })
+    const indexPage = Page.verifyOnPage(IndexPage)
+    let varyApproveCasesPage = indexPage.clickApproveAVariation()
+    const varyApproveViewPage = varyApproveCasesPage.selectCase()
+    const varyReferPage = varyApproveViewPage.clickReferVariation()
+    const varyReferConfirmPage = varyReferPage
+      .enterReasonForReferral('This case requires more restrictive conditions on movement please')
+      .expandVariationDetails()
+      .checkHdcCurfewDetails('123 Fake Street, Apt 4, Fakecounty, FK1 2AB', true)
+      .clickConfirmReferral()
+    varyApproveCasesPage = varyReferConfirmPage.clickBackToCaseList()
     varyApproveCasesPage.signOut().click()
   })
 })
