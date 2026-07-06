097344f9980a3c9a6af57004d983455b34c6f722
Gurps Singh <60613823+gurps-singh@users.noreply.github.com>
2026-07-06 16:38:17 +0100

    CVSL-4066 remove text from approver screens (#1880)

--- server/routes/approvingLicences/handlers/confirmApproved.ts
+++ server/routes/approvingLicences/handlers/confirmApproved.ts
@@ -16,17 +16,9 @@ export default class ConfirmApprovedRoutes {
     const fullName = convertToTitleCase(`${licence.forename || ''} ${licence.surname || ''}`.trim())
 
     switch (licence.typeCode) {
-      case LicenceType.AP_PSS:
-        titleText = `Licence and post sentence supervision order approved`
-        confirmationMessage = `A case administrator can now print the licence and post sentence supervision order for ${fullName}.`
-        break
       case LicenceType.AP:
         titleText = `Licence approved`
         confirmationMessage = `A case administrator can now print the licence for ${fullName}.`
-        break
-      case LicenceType.PSS:
-        titleText = `Post sentence supervision order approved`
-        confirmationMessage = `A case administrator can now print the post sentence supervision order for ${fullName}.`
         break
       default: {
         // silently ignore
