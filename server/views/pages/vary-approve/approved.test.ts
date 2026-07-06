1a01701fede6a9af44560069e631075c97a622d6
nishanthmahasamudram <nishanth.mahasamudram@justice.gov.uk>
2026-07-06 17:28:15 +0100

    CVSL-4317 Remove red highlight from PP search and show HDCAD release label

--- server/views/pages/vary-approve/approved.test.ts
+++ server/views/pages/vary-approve/approved.test.ts
@@ -29,4 +29,28 @@ describe('Vary approve approved page', () => {
       'The variation for Hale Draven Corvath has been approved with your digital signature.',
     )
   })
+
+  it('should render offender name without undefined when surname is missing', () => {
+    const $ = render({
+      licence: {
+        forename: 'hAle',
+      },
+      applicationName: 'Create and vary a licence',
+    })
+
+    expect($('.govuk-panel__title').text()).toContain('Variation for Hale approved')
+    expect($('#sent-to').text()).toContain('The variation for Hale has been approved with your digital signature.')
+  })
+
+  it('should render print and issue licence guidance', () => {
+    const $ = render({
+      licence: {
+        forename: 'Hale',
+        surname: 'Corvath',
+      },
+      applicationName: 'Create and vary a licence',
+    })
+
+    expect($('.govuk-body').text()).toContain('They can then print and issue the licence.')
+  })
 })
