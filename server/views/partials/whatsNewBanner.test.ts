7e88bea34ca6cf8e1bbcb8d87ded481cd2e337bd
Gurps Singh <gurpreet.singh@justice.gov.uk>
2026-07-23 10:34:13 +0100

    CVSL-4350 change content for comms banner for probation user

--- server/views/partials/whatsNewBanner.test.ts
+++ server/views/partials/whatsNewBanner.test.ts
@@ -24,4 +24,18 @@ describe('Whats new banner', () => {
     })
     expect($('body').text()).not.toContain(`Upcoming changes to licence conditions`)
   })
+
+  it('Show whats new banner with probation staff message if isProbationStaff is true', () => {
+    const $ = render({
+      showCommsBanner: true,
+      isProbationUser: true,
+    })
+    expect($('body').text()).toContain('Upcoming changes to licence conditions')
+    expect($('body').text()).toContain(
+      'Probation practitioners can start using the new additional conditions for these people from 27 July 2026. The new additional conditions will be available when varying licences from 2 September.',
+    )
+    expect($('body').text()).toContain(
+      'Standard conditions for anyone released on or after 2 September will be updated automatically.',
+    )
+  })
 })
