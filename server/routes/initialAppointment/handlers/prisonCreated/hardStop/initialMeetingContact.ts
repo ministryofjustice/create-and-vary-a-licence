869e88d4745bbdbc920f5a81367d302a9ff13a99
Gurps Singh <60613823+gurps-singh@users.noreply.github.com>
2026-07-28 11:10:09 +0100

    CVSL-4282 update contact number page for omu (#1933)

--- server/routes/initialAppointment/handlers/prisonCreated/hardStop/initialMeetingContact.ts
+++ server/routes/initialAppointment/handlers/prisonCreated/hardStop/initialMeetingContact.ts
@@ -12,9 +12,13 @@ export default class InitialMeetingContactRoutes {
 
   GET = async (req: Request, res: Response): Promise<void> => {
     const { edit } = req.query
+    const { licence } = res.locals
+    const noAppointmentNeeded = licence.appointmentPersonType === 'NO_APPOINTMENT_NEEDED'
+
     return res.render('pages/initialAppointment/prisonCreated/initialMeetingContact', {
       continueOrSaveLabel: this.path === PathType.EDIT ? 'Save' : 'Continue',
       edit,
+      noAppointmentNeeded,
     })
   }
 
@@ -24,7 +28,7 @@ export default class InitialMeetingContactRoutes {
     await this.licenceService.updateContactNumber(licenceId, req.body, user)
 
     flashInitialApptUpdatedMessage(req, licence, UserType.PRISON)
-    if (this.path === PathType.EDIT) {
+    if (this.path === PathType.EDIT || licence.appointmentPersonType === 'NO_APPOINTMENT_NEEDED') {
       res.redirect(`/licence/hard-stop/id/${licenceId}/check-your-answers`)
     } else {
       res.redirect(`/licence/hard-stop/create/id/${licenceId}/initial-meeting-time`)
