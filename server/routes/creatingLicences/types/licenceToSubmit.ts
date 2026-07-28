d7c3c928ba1366b3776de7b8bd78e4e266c59a3d
dave-virden <151018159+dave-virden@users.noreply.github.com>
2026-07-28 15:22:37 +0100

    CVSL-4255 Update edit links for no appointment needed.

--- server/routes/creatingLicences/types/licenceToSubmit.ts
+++ server/routes/creatingLicences/types/licenceToSubmit.ts
@@ -8,7 +8,7 @@ import ElectronicMonitoringProvider from '../../manageConditions/types/electroni
 class LicenceToSubmit {
   @Expose()
   @IsNotEmpty({ message: "Select 'Change' to go back and add who to meet" })
-  appointmentPersonType: 'DUTY_OFFICER' | 'RESPONSIBLE_COM' | 'SPECIFIC_PERSON'
+  appointmentPersonType: 'DUTY_OFFICER' | 'RESPONSIBLE_COM' | 'SPECIFIC_PERSON' | 'NO_APPOINTMENT_NEEDED'
 
   @Expose()
   @ValidateIf(o => o.appointmentPersonType === 'SPECIFIC_PERSON')
