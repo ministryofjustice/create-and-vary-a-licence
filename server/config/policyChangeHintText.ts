99e00d2cfa8a47f056736832a40376d353dcbf6e
Seb Negahban <seb.negahban@digital.justice.gov.uk>
2026-08-18 11:25:55 +0100

    CVSL-4324 Adding hint text to esim policy upgrade

--- server/config/policyChangeHintText.ts
+++ server/config/policyChangeHintText.ts
@@ -201,6 +201,12 @@ const policyChangeHints: HintText[] = [
     ],
     bulletpoints: [],
   },
+  {
+    code: '8e52e16e-1abf-4251-baca-2fabfcb243d0',
+    fromVersions: ['1.0', '2.0', '2.1', '3.0'],
+    description: ['Requirement not to install an e-sim added.'],
+    bulletpoints: [],
+  },
 ]
 
 export default policyChangeHints
