81bfa6209572605690ba19c5d6a35223e0bbb328
Nishanth <129746957+nishanthmahasamudram@users.noreply.github.com>
2026-08-25 13:13:18 +0100

    CVSL-4466 Create Polygraph upgrade page V4 (#2009)

--- server/config/policyChangeHintText.ts
+++ server/config/policyChangeHintText.ts
@@ -216,6 +216,12 @@ const policyChangeHints: HintText[] = [
     bulletpoints: [],
   },
   {
+    code: '1dc7ee29-df47-48a8-90b6-69e286692d8a',
+    fromVersions: ['1.0', '2.0', '2.1', '3.0'],
+    description: ['The condition has been simplified.'],
+    bulletpoints: [],
+  },
+  {
     code: 'fd129172-bdd3-4d97-a4a0-efd7b47a49d4',
     fromVersions: ['1.0', '2.0', '2.1', '3.0'],
     description: [],
